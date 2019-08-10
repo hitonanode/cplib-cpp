3acef64ff102c2f2840d4e26d89e55c739627f27
rsm9 <32937551+rsm9@users.noreply.github.com>
2019-08-10 10:25:55 +0900

    initial

---
+++ unionfind/unionfind_plain.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#include <numeric>
+#include <vector>
+
+struct UnionFind
+{
+    std::vector<int> par, rank;
+    UnionFind(int N = 0) {
+        par = rank = std::vector<int>(N);
+        iota(par.begin(), par.end(), 0);
+    }
+    int find(int x) { return (par[x] == x) ? x : (par[x] = find(par[x])); }
+    bool unite(int x, int y) {
+        x = find(x), y = find(y);
+        if (x == y) return false;
+        if (rank[x] < rank[y]) par[x] = y;
+        else par[y] = x;
+        if (rank[x] == rank[y]) rank[x]++;
+        return true;
+    }
+    bool same(int x, int y) { return find(x) == find(y); }
+};
