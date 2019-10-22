fdc05876f174b931c57d8bdd2385e54b714e6a85
rsm9 <32937551+rsm9@users.noreply.github.com>
2019-10-22 22:40:55 +0900

    LCA_tree

--- convolution/integer_convolution/integer_convolution.hpp
+++ convolution/integer_convolution/integer_convolution.hpp
@@ -4,7 +4,7 @@ using lint = long long int;
 
 using T = lint;
 
-// f[n]に対して、全てのnの倍数n*iに対するf[n*i]の和が出てくる 計算量O(NlogN)
+// f[n]のn >= 1に対して、全ての倍数n*iに対するf[n*i]の和が出てくる 計算量O(NlogN)
 void integer_zeta(vector<T> &f)
 {
     int N = f.size() - 1;
@@ -30,4 +30,17 @@ void fast_integer_zeta(vector<T> &f)
         for (int j = N / p * p; j > 0; j -= p) f[j / p] += f[j];
     }
     
+}
+
+// fast_integer_zetaの逆演算 O(NloglogN)
+// 使用例 <https://yukicoder.me/submissions/385120>
+void fast_integer_moebius(vector<T> &f)
+{
+    int N = f.size() - 1;
+    vector<int> is_prime(N + 1, 1);
+    for (int p = 2; p <= N; p++) if (is_prime[p])
+    {
+        for (int q = p * 2; q <= N; q += p) is_prime[q] = 0;
+        for (int j = p; j <= N; j += p) f[j / p] -= f[j];
+    }
 }
---
+++ graph-tree/lowest_common_ancestor.hpp
@@ -0,0 +1,98 @@
+// lowest common ancestor (LCA) class for undirected weighted tree
+// 無向重み付きグラフの最小共通祖先
+// <https://yukicoder.me/submissions/392383>
+
+#include <vector>
+using namespace std;
+
+class UndirectedWeightedTree
+{
+    using T = long long int;   // Arbitrary data structure (operator+, operator- must be defined)
+    const int INVALID = -1;
+    int V, lgV;
+    int E;
+    int root;
+    vector<vector<pair<int, int>>> adj; // (nxt_vertex, edge_id)
+    // vector<pint> edge;  // edges[edge_id] = (vertex_id, vertex_id)
+    vector<T> weight;  // w[edge_id]
+    vector<int> par;  // parent_vertex_id[vertex_id]
+    vector<int> depth;  // depth_from_root[vertex_id]
+    vector<T> acc_weight;  // w_sum_from_root[vertex_id]
+
+    void _fix_root_dfs(int now, int prv, int prv_edge_id)
+    {
+        par[now] = prv;
+        if (prv_edge_id != INVALID) acc_weight[now] = acc_weight[prv] + weight[prv_edge_id];
+        for (auto nxt : adj[now]) if (nxt.first != prv)
+        {
+            depth[nxt.first] = depth[now] + 1;
+            _fix_root_dfs(nxt.first, now, nxt.second);
+        }
+    }
+
+public:
+    UndirectedWeightedTree(int N = 0): V(N), E(0), adj(N) {
+        lgV = 1;
+        while (1 << lgV < V) lgV++;
+    }
+
+    void add_edge(int u, int v, T w)
+    {
+        adj[u].emplace_back(v, E);
+        adj[v].emplace_back(u, E);
+        // edge.emplace_back(u, v);
+        weight.emplace_back(w);
+        E++;
+    }
+
+    void fix_root(int r)
+    {
+        root = r;
+        par.resize(V);
+        depth.assign(V, 0);
+        acc_weight.resize(V);
+        _fix_root_dfs(root, INVALID, INVALID);
+    }
+
+    vector<vector<int>> doubling;
+    void doubling_precalc()
+    {
+        doubling.assign(lgV, vector<int>(V));
+        doubling[0] = par;
+        for (int d = 0; d < lgV - 1; d++) for (int i = 0; i < V; i++)
+        {
+            if (doubling[d][i] == INVALID) doubling[d + 1][i] = INVALID;
+            else doubling[d + 1][i] = doubling[d][doubling[d][i]];
+        }
+    }
+
+    int kth_parent(int x, int k)
+    {
+        for (int d = 0; d < lgV; d++)
+        {
+            if (x == INVALID) return INVALID;
+            if (k & (1 << d)) x = doubling[d][x];
+        }
+        return x;
+    }
+
+    int lowest_common_ancestor(int u, int v)
+    {
+        if (depth[u] > depth[v]) swap(u, v);
+
+        v = kth_parent(v, depth[v] - depth[u]);
+        if (u == v) return u;
+        for (int d = lgV - 1; d >= 0; d--)
+        {
+            if (doubling[d][u] != doubling[d][v]) u = doubling[d][u], v = doubling[d][v];
+        }
+        return par[u];
+    }
+
+    T path_length(int u, int v)
+    {
+        // Not distance, but the sum of weights
+        int r = lowest_common_ancestor(u, v);
+        return (acc_weight[u] - acc_weight[r]) + (acc_weight[v] - acc_weight[r]);
+    }
+};
