f7ac060b0d3f84a574c77bebd73dd9d823d78f9b
rsm9 <32937551+rsm9@users.noreply.github.com>
2019-12-08 16:56:39 +0900

    timestamp

---
+++ modulus/test/sqrt_modint_runtime.test.cpp
@@ -0,0 +1,21 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/sqrt_mod"
+#include <iostream>
+#include "modulus/modint_runtime.hpp"
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+    while (T--) {
+        int Y, P;
+        cin >> Y >> P;
+        ModIntRuntime::mod = P;
+        ModIntRuntime m = Y;
+        if (Y) {
+            m = m.sqrt();
+            cout << (m ? m.val : -1) << endl;
+        }
+        else cout << 0 << endl;
+    }
+}
