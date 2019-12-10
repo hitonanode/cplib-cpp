bc6da77e2b7d7ac46506ff7e74f8649119c76c18
rsm9 <32937551+rsm9@users.noreply.github.com>
2019-12-10 23:49:20 +0900

    Add test for scc at AOJ

---
+++ graph/test/strongly_connected_components.test.cpp
@@ -0,0 +1,28 @@
+#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_3_C"
+#include "graph/strongly_connected_components.hpp"
+
+#include <stdio.h>
+
+int main()
+{
+    int V, E;
+    scanf("%d %d", &V, &E);
+    DirectedGraphSCC graph(V);
+    while (E--)
+    {
+        int s, t;
+        scanf("%d %d", &s, &t);
+        graph.add_edge(s, t);
+    }
+
+    graph.FindStronglyConnectedComponents();
+
+    int Q;
+    scanf("%d", &Q);
+    while (Q--)
+    {
+        int u, v;
+        scanf("%d %d", &u, &v);
+        printf("%d\n", graph.cmp[u] == graph.cmp[v]);
+    }
+}
