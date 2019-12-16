5e4f0bfbde0ac5ac437071e7bb9dd75ee88a0667
rsm9 <32937551+rsm9@users.noreply.github.com>
2019-12-16 14:55:59 +0900

    convex hull trick (monotone query)

---
+++ convex_hull_trick/monotone_cht.hpp
@@ -0,0 +1,37 @@
+#pragma once
+#include <deque>
+#include <utility>
+
+// Convex Hull Trick for monotone increasing queries, monotone decreasing slopes
+// Each operation is amortized O(1)
+// - add_line(a, b): Add `y = ax + b`, a must be monotone decreasing
+// - add_convex_parabola(c, a, b): Add `y = c(x - a)^2 + b` (c is constant & a is monotone increasing)
+// - get(x): Calculate min/max. value of `y = ax + b`'s at point x, x must be monotone increasing
+// - parabola_get(c, x): Caclculate min/max. value of `y = c(x - a)^2 + b`'s
+// Verified: <https://yukicoder.me/submissions/409156>
+using T_CHT = long long int; // template<typename T_CHT>
+struct MonotoneConvexHullTrick : std::deque<std::pair<T_CHT, T_CHT>> // (a, b) means `y = ax + b`
+{
+    using T_MP = __int128_t;
+    bool is_minimizer; // If true, this instance calculates min. Otherwise, calculates max.
+    MonotoneConvexHullTrick(bool is_minimizer) : is_minimizer(is_minimizer) {}
+    void add_line(T_CHT a, T_CHT b) {  // Add y = ax + b
+        if (!is_minimizer) a = -a, b = -b;
+        while (this->size() > 1u) {
+            int sz = this->size();
+            T_MP l = (T_MP)(this->back().second - (*this)[sz - 2].second) * (this->back().first - a); // Overflow might occur here.
+            T_MP r = (T_MP)(b - this->back().second) * ((*this)[sz - 2].first - this->back().first);
+            if (l < r) break;
+            this->pop_back();
+        }
+        this->emplace_back(a, b);
+    }
+    T_CHT get(T_CHT x) { 
+        while (this->size() > 1u and (*this)[0].first * x + (*this)[0].second > (*this)[1].first * x + (*this)[1].second) {
+            this->pop_front();
+        }
+        return ((*this)[0].first * x + (*this)[0].second) * (is_minimizer ? 1 : -1);
+    }
+    void add_convex_parabola(T_CHT c, T_CHT a, T_CHT b) { add_line(-2 * c * a, c * a * a + b); } // Add y = c(x - a)^2 + b
+    T_CHT parabola_get(T_CHT c, T_CHT x) { return get(x) + c * x * x; }
+};
