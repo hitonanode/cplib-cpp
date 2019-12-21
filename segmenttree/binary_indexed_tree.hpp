865c82fb3b14edfa164b5778cf6f12cf5895a486
rsm9 <32937551+rsm9@users.noreply.github.com>
2019-12-22 01:52:53 +0900

    1d binary indexed tree

---
+++ segmenttree/binary_indexed_tree.hpp
@@ -0,0 +1,30 @@
+#pragma once
+#include <iostream>
+#include <vector>
+
+// 1-indexed BIT (i : [1, len])
+template<typename T>
+struct BIT : std::vector<T>
+{
+    BIT(int len = 0) : std::vector<T>(len + 1) {}
+    void reset() { fill(this->begin(), this->end(), 0); }
+    void add(int pos, T v) {
+        while (pos > 0 and pos < (int)this->size()) (*this)[pos] += v, pos += pos & -pos;
+    }
+    T sum(int pos) const { // (0, pos]
+        T res = 0;
+        while (pos > 0) res += (*this)[pos], pos -= pos & -pos;
+        return res;
+    }
+    friend std::ostream &operator<<(std::ostream &os, const BIT &bit) {
+        T prv = 0;
+        os << '[';
+        for (int i = 1; i < (int)bit.size(); i++) {
+            T now = bit.sum(i);
+            os << now - prv << ",";
+            prv = now;
+        }
+        os << ']';
+        return os;
+    }
+};
---
+++ segmenttree/test/binary_indexed_tree.test.cpp
@@ -0,0 +1,17 @@
+#include "segmenttree/binary_indexed_tree.hpp"
+#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_2_B"
+#include <cstdio>
+
+int main()
+{
+    int N, Q;
+    scanf("%d %d", &N, &Q);
+    BIT<int> bit(N);
+    while (Q--)
+    {
+        int c, x, y;
+        scanf("%d %d %d", &c, &x, &y);
+        if (c) printf("%d\n", bit.sum(y) - bit.sum(x - 1));
+        else bit.add(x, y);
+    }
+}
