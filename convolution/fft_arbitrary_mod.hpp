92e1d0d491b52573cdfca6654babb676e17fb17f
rsm9 <32937551+rsm9@users.noreply.github.com>
2020-01-13 15:48:40 +0900

    fft long double

--- convolution/fft_arbitrary_mod.hpp
+++ convolution/fft_arbitrary_mod.hpp
@@ -5,11 +5,14 @@
 using namespace std;
 
 // Arbitrary mod (<1e9) FFT/convolution
+// MAXA*MAXB*N <= 1e15 (double), 1e19 (long double)
 // Based on <https://ei1333.github.io/luzhiled/snippets/math/arbitrary-mod-convolution.html>
+using T_FFT = long double;
+constexpr int D_FFT = 15;
 struct cmplx{
-    double x, y;
+    T_FFT x, y;
     cmplx() : x(0), y(0) {}
-    cmplx(double x, double y) : x(x), y(y) {}
+    cmplx(T_FFT x, T_FFT y) : x(x), y(y) {}
     inline cmplx operator+(const cmplx &r) const { return cmplx(x + r.x, y + r.y); }
     inline cmplx operator-(const cmplx &r) const { return cmplx(x - r.x, y - r.y); }
     inline cmplx operator*(const cmplx &r) const { return cmplx(x * r.x - y * r.y, x * r.y + y * r.x); }
@@ -26,10 +29,10 @@ void ensure_base(int nbase) {
         fftrev[i] = (fftrev[i >> 1] >> 1) + ((i & 1) << (nbase - 1));
     }
     while (fftbase < nbase) {
-        double angle = M_PI * 2.0 / (1 << (fftbase + 1));
+        T_FFT angle = acosl(-1.0L) * 2.0L / (1 << (fftbase + 1));
         for (int i = 1 << (fftbase - 1); i < (1 << fftbase); i++) {
             fftrts[i << 1] = fftrts[i];
-            double angle_i = angle * (2 * i + 1 - (1 << fftbase));
+            T_FFT angle_i = angle * (2 * i + 1 - (1 << fftbase));
             fftrts[(i << 1) + 1] = {cos(angle_i), sin(angle_i)};
         }
         ++fftbase;
@@ -67,15 +70,15 @@ vector<MODINT> convolution_mod(vector<MODINT> a, vector<MODINT> b)
     while ((1 << nbase) < need) nbase++;
     int sz = 1 << nbase;
     vector<cmplx> fa(sz);
-    for (int i = 0; i < (int)a.size(); i++) fa[i] = {double(a[i].val & ((1 << 15) - 1)), double(a[i].val >> 15)};
+    for (int i = 0; i < (int)a.size(); i++) fa[i] = {(T_FFT)(a[i].val & ((1LL << D_FFT) - 1)), (T_FFT)(a[i].val >> D_FFT)};
     fft(sz, fa);
     vector<cmplx> fb(sz);
     if (a == b) fb = fa;
     else {
-        for (int i = 0; i < (int)b.size(); i++) fb[i] = {double(b[i].val & ((1 << 15) - 1)), double(b[i].val >> 15)};
+        for (int i = 0; i < (int)b.size(); i++) fb[i] = {(T_FFT)(b[i].val & ((1LL << D_FFT) - 1)), (T_FFT)(b[i].val >> D_FFT)};
         fft(sz, fb);
     }
-    double ratio = 0.25 / sz;
+    T_FFT ratio = 0.25L / sz;
     cmplx r2(0, -1), r3(ratio, 0), r4(0, -ratio), r5(0, 1);
     for (int i = 0; i <= (sz >> 1); i++) {
         int j = (sz - i) & (sz - 1);
@@ -97,12 +100,15 @@ vector<MODINT> convolution_mod(vector<MODINT> a, vector<MODINT> b)
     fft(sz, fa);
     fft(sz, fb);
     vector<MODINT> ret(sz);
+    long long int bp = MODINT(2).power(D_FFT);
+    long long int cp = MODINT(2).power(D_FFT * 2);
     for (int i = 0; i < need; i++) {
-      int64_t aa = llround(fa[i].x);
-      int64_t bb = llround(fb[i].x);
-      int64_t cc = llround(fa[i].y);
-      aa = MODINT(aa).val, bb = MODINT(bb).val, cc = MODINT(cc).val;
-      ret[i] = aa + (bb << 15) + (cc << 30);
+        long long int aa = llround(fa[i].x);
+        long long int bb = llround(fb[i].x);
+        long long int cc = llround(fa[i].y);
+        aa = MODINT(aa).val, bb = MODINT(bb).val, cc = MODINT(cc).val;
+        ret[i] = aa + (bb * bp) + (cc * cp);
+        //   ret[i] = aa + (bb << DFFT) + (cc << (DFFT * 2));
     }
     return ret;
 }
