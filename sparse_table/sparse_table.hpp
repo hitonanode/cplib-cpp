93ecfac286d31ffe3cd89d587e34b3f4b6a61868
rsm9 <32937551+rsm9@users.noreply.github.com>
2020-02-15 16:21:57 +0900

    sparse table

---
+++ sparse_table/sparse_table.hpp
@@ -0,0 +1,36 @@
+#pragma once
+#include <algorithm>
+#include <cassert>
+#include <vector>
+
+// Range Minimum Query for static sequence by sparse table
+// Complexity: O(NlogN) for precalculation, O(1) per query
+template<typename T>
+struct SparseTableRMQ {
+    inline T func(const T &l, const T &r) {
+        return std::min<T>(l, r);
+    }
+    int N, lgN;
+    T defaultT;
+    std::vector<std::vector<T>> data;
+    std::vector<int> lgx_table;
+    SparseTableRMQ(const std::vector<T> &sequence, T defaultT) : N(sequence.size()), defaultT(defaultT)
+    {
+        lgx_table.resize(N + 1);
+        for (int i = 2; i < N + 1; i++) lgx_table[i] = lgx_table[i >> 1] + 1;
+        lgN = lgx_table[N] + 1;
+        data.assign(lgN, std::vector<T>(N, defaultT));
+        data[0] = sequence;
+        for (int d = 1; d < lgN; d++) {
+            for (int i = 0; i + (1 << d) <= N; i++) {
+                data[d][i] = func(data[d - 1][i], data[d - 1][i + (1 << (d - 1))]);
+            }
+        }
+    }
+    T get(int l, int r) { // [l, r), 0-indexed
+        assert(l >= 0 and r <= N);
+        if (l >= r) return defaultT;
+        int d = lgx_table[r - l];
+        return func(data[d][l], data[d][r - (1 << d)]);
+    }
+};
---
+++ sparse_table/test/sparse_table.test.cpp
@@ -0,0 +1,17 @@
+#include "sparse_table/sparse_table.hpp"
+#include <cstdio>
+#define PROBLEM "https://judge.yosupo.jp/problem/staticrmq"
+
+int main()
+{
+    int N, Q;
+    scanf("%d %d", &N, &Q);
+    std::vector<int> A(N);
+    for (int i = 0; i < N; i++) scanf("%d", &A[i]);
+    SparseTableRMQ<int> rmq(A, 2e9);
+    while (Q--) {
+        int l, r;
+        scanf("%d %d", &l, &r);
+        printf("%d\n", rmq.get(l, r));
+    }
+}
