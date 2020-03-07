c9743cc3f544e2bd5c089d593bc042771d600158
rsm9 <32937551+rsm9@users.noreply.github.com>
2020-03-07 12:37:26 +0900

    Enumerate Divisions

---
+++ number/enumerate_partitions.hpp
@@ -0,0 +1,36 @@
+#include <algorithm>
+#include <numeric>
+#include <vector>
+
+// Enumerate Partitions of number <= N （自然数の分割の列挙）
+// - can be used for N less than ~50
+// - Example:
+//   - 1 => [[1,],]
+//   - 2 => [[1,],[1,1,],[2,],]
+//   - 3 => [[1,],[1,1,],[1,1,1,],[2,],[2,1,],[3,],]
+struct
+{
+    std::vector<std::vector<int>> ret;
+    std::vector<int> now;
+    int num;
+    void _partition_dfs() {
+        int rem = num - std::accumulate(now.begin(), now.end(), 0);
+        if (!now.empty()) rem = std::min(rem, now.back());
+
+        now.push_back(0);
+        for (int j = 1; j <= rem; j++) {
+            now.back() = j;
+            ret.push_back(now);
+            _partition_dfs();
+        }
+        now.pop_back();
+    }
+
+    std::vector<std::vector<int>> operator()(int N) {
+        ret.clear();
+        now.clear();
+        num = N;
+        _partition_dfs();
+        return ret;
+    }
+} EnumeratePartitions;
