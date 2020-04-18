70e5ce322dcbbc739646f80f46e7042ba3e9e50c
rsm9 <32937551+rsm9@users.noreply.github.com>
2020-04-19 04:33:31 +0900

    fast Pollard-rho

--- number/factorize.hpp
+++ number/factorize.hpp
@@ -1,63 +1,93 @@
 #pragma once
+#include <array>
 #include <algorithm>
 #include <cassert>
 #include <vector>
 
+namespace SPRP
+{
+// <http://miller-rabin.appspot.com/>
+const std::vector<std::vector<__int128>> bases{
+    {126401071349994536},                              // <= 291831
+    {336781006125, 9639812373923155},                  // <= 1050535501 (1e9)
+    {2, 2570940, 211991001, 3749873356},               // <= 47636622961201 (4e13)
+    {2, 325, 9375, 28178, 450775, 9780504, 1795265022} // <= 1e64
+};
+inline int get_id(long long n)
+{
+    if (n <= 291831) return 0;
+    else if (n <= 1050535501) return 1;
+    else if (n <= 47636622961201) return 2;
+    else return 3;
+}
+} // namespace SPRP
+
+
+// Miller-Rabin primality test
+// <https://ja.wikipedia.org/wiki/%E3%83%9F%E3%83%A9%E3%83%BC%E2%80%93%E3%83%A9%E3%83%93%E3%83%B3%E7%B4%A0%E6%95%B0%E5%88%A4%E5%AE%9A%E6%B3%95>
+// Complexity: O(lg n) per query
 struct
 {
-    long long modpow(__int128 x, __int128 n, long long mod)
+    long long modpow(__int128 x, __int128 n, long long mod) noexcept
     {
-        __int128 ans = 1;
-        while (n)
-        {
-            if (n & 1) (ans *= x) %= mod;
-            (x *= x) %= mod;
-            n >>= 1;
-        }
-        return ans;
+        __int128 ret = 1;
+        for (x %= mod; n; x = x * x % mod, n >>= 1) ret = (n & 1) ? ret * x % mod : ret;
+        return ret;
     }
-    // Miller-Rabin primality test
-    // <https://ja.wikipedia.org/wiki/%E3%83%9F%E3%83%A9%E3%83%BC%E2%80%93%E3%83%A9%E3%83%93%E3%83%B3%E7%B4%A0%E6%95%B0%E5%88%A4%E5%AE%9A%E6%B3%95>
-    bool is_prime(long long n)
+    bool operator()(long long n) noexcept
     {
         if (n < 2) return false;
         if (n % 2 == 0) return n == 2;
         int s = __builtin_ctzll(n - 1);
-        long long d = (n - 1) >> s;
-        // <http://miller-rabin.appspot.com/>
-        for (auto a : {2, 325, 9375, 28178, 450775, 9780504, 1795265022})
+
+        for (__int128 a : SPRP::bases[SPRP::get_id(n)])
         {
-            __int128 ad = modpow(a, d, n);
+            a = modpow(a, (n - 1) >> s, n);
             bool may_composite = true;
-            if (ad <= 1) continue;
-            for (int r = 0; r < s; r++, ad = ad * ad % n) if (ad == n - 1) may_composite = false;
+            if (!a) return true;
+            if (a == 1) continue;
+            for (int r = s; r--; a = a * a % n) if (a == n - 1) may_composite = false;
             if (may_composite) return false;
         }
         return true;
     }
+} is_prime;
 
+struct
+{
     // Pollard's rho algorithm: find factor greater than 1
     long long find_factor(long long n)
     {
         assert(n > 1);
         if (n % 2 == 0) return 2;
         if (is_prime(n)) return n;
-        long long x, y, d, c = 1;
+        long long c = 1;
         auto f = [&](__int128 x) -> long long { return (x * x + c) % n; };
-        for (;; c++)
-        {
-            x = 2, y = 2, d = 1;
-            while (d == 1) {
-                x = f(x);
-                y = f(f(y));
-                d = std::__gcd(std::abs(x - y), n);
+
+        for (int t = 1;; t++) {
+            long long x0 = t, m = std::max(n >> 3, 1LL), x, ys, y = x0, r = 1, g, q = 1;
+            do {
+                x = y;
+                for (int i = r; i--;) y = f(y);
+                long long k = 0;
+                do {
+                    ys = y;
+                    for (int i = std::min(m, r - k); i--;) y = f(y), q = __int128(q) * std::abs(x - y) % n;
+                    g = std::__gcd<long long>(q, n);
+                    k += m;
+                } while (k < r and g <= 1);
+                r <<= 1;
+            } while (g <= 1);
+            if (g == n) {
+                do {
+                    ys = f(ys);
+                    g = std::__gcd(std::abs(x - ys), n);
+                } while (g <= 1);
             }
-            if (d < n) break;
+            if (g != n) return g;
         }
-        return d;
     }
 
-    // not sorted
     std::vector<long long> operator()(long long n)
     {
         std::vector<long long> ret;
