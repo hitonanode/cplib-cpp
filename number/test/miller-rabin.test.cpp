88d3c7f4b79b45182845c29c4a0be939b37a9d34
rsm9 <32937551+rsm9@users.noreply.github.com>
2020-04-19 04:48:57 +0900

    Miller-Rabin test

---
+++ number/test/miller-rabin.test.cpp
@@ -0,0 +1,14 @@
+#include "number/factorize.hpp"
+#define PROBLEM "https://yukicoder.me/problems/1967"
+#include <iostream>
+
+int main()
+{
+    int Q;
+    std::cin >> Q;
+    while (Q--) {
+        long long n;
+        std::cin >> n;
+        std::cout << n << ' ' << is_prime(n) << '\n';
+    }
+}
