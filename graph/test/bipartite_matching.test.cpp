a82e35b217455125a552de7c7fff97b0877dc133
rsm9 <32937551+rsm9@users.noreply.github.com>
2020-04-18 19:20:50 +0900

    Bipartite matching color_bfs

--- graph/test/bipartite_matching.test.cpp
+++ graph/test/bipartite_matching.test.cpp
@@ -1,6 +1,10 @@
 #include "graph/bipartite_matching.hpp"
 #define PROBLEM "https://judge.yosupo.jp/problem/bipartitematching"
 
+#include <algorithm>
+#include <numeric>
+#include <random>
+
 int main()
 {
     std::cin.tie(NULL);
@@ -8,15 +12,29 @@ int main()
 
     int L, R, M;
     std::cin >> L >> R >> M;
-    BipartiteMatching bm(L, R);
+
+    std::vector<int> perm(L + R), perm_inv(L + R);
+    std::iota(perm.begin(), perm.end(), 0);
+    std::random_device rd;
+    std::mt19937 g(rd());
+    std::shuffle(perm.begin(), perm.end(), g);
+    for (int i = 0; i < L + R; i++)
+    {
+        perm_inv[perm[i]] = i;
+    }
+
+    BipartiteMatching bm(L + R);
     while (M--)
     {
         int a, b;
         std::cin >> a >> b;
-        bm.add_edge(a, b);
+        bm.add_edge(perm[a], perm[L + b]);
     }
+
     std::cout << bm.solve() << '\n';
-    for (int j = 0; j < R; j++) if (bm.match[j] >= 0) {
-        std::cout << bm.match[j] << ' ' << j << '\n';
+    for (int j = 0; j < L + R; j++) if (bm.match[j] > j) {
+        int a = perm_inv[j], b = perm_inv[bm.match[j]];
+        if (a > b) std::cout << b << ' ' << a - L << '\n';
+        else std::cout << a << ' ' << b - L << '\n';
     }
 }
