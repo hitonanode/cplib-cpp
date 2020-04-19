052db723f0f3d6e05530241ccf18c9b6d7be98e5
rsm9 <32937551+rsm9@users.noreply.github.com>
2020-04-19 16:51:53 +0900

    CHT refactoring

--- convex_hull_trick/test/convex_hull_trick.test.cpp
+++ convex_hull_trick/test/convex_hull_trick.test.cpp
@@ -2,31 +2,33 @@
 #include <iostream>
 #include <cassert>
 #define PROBLEM "https://judge.yosupo.jp/problem/line_add_get_min"
-using namespace std;
 
-ConvexHullTrick cht_min(true), cht_max(false);
+ConvexHullTrick<__int128> cht_min(true), cht_max(false);
 void add_line()
 {
     long long int a, b;
-    cin >> a >> b;
+    std::cin >> a >> b;
     cht_min.add_line(a, b);
     cht_max.add_line(-a, -b);
 }
 int main()
 {
+    std::cin.tie(NULL);
+    std::ios::sync_with_stdio(false);
+
     int N, Q;
-    cin >> N >> Q;
+    std::cin >> N >> Q;
     while (N--) add_line();
     while (Q--) {
         int q;
-        cin >> q;
+        std::cin >> q;
         if (q) {
             int p;
-            cin >> p;
+            std::cin >> p;
             auto ret1 = cht_min.get(p);
             auto ret2 = cht_max.get(p);
             assert(ret1.first == -ret2.first);
-            cout << ret1.first << endl;
+            std::cout << ret1.first << '\n';
         }
         else add_line();
     }
