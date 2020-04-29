41dad05cde08000ba79e3551537f6218466a7455
rsm9 <32937551+rsm9@users.noreply.github.com>
2020-04-30 02:04:57 +0900

    fibonacci heap

---
+++ other_data_structures/test/fibonacci_heap_dijkstra.test.cpp
@@ -0,0 +1,39 @@
+#include "other_data_structures/fibonacci_heap.hpp"
+#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_1_A"
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    int V, E, R;
+    cin >> V >> E >> R;
+
+    using lint = long long int;
+    using plint = pair<lint, lint>;
+    heap<lint> fib(V, 1e18);
+    fib.chmin(R, 0);
+
+    vector<vector<plint>> to(V);
+    while (E--)
+    {
+        int s, t, d;
+        cin >> s >> t >> d;
+        to[s].emplace_back(t, d);
+    }
+    while (fib.size())
+    {
+        int now = fib.pop().second;
+        for (auto p : to[now]) fib.chmin(p.first, fib[now] + p.second);
+    }
+
+    for (auto x : fib.result)
+    {
+        if (x == 1e18) cout << "INF\n";
+        else cout << x << '\n';
+    }
+}
---
+++ other_data_structures/test/fibonacci_heap_spanningtree.test.cpp
@@ -0,0 +1,40 @@
+#include "other_data_structures/fibonacci_heap.hpp"
+#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_2_A"
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    int V, E;
+    cin >> V >> E;
+    fibonacci_heap<pair<int, int>> hp;
+
+    vector<vector<pair<int, int>>> to(V);
+    vector<int> belong(V);
+    while (E--)
+    {
+        int s, t, w;
+        cin >> s >> t >> w;
+        to[s].emplace_back(w, t);
+        to[t].emplace_back(w, s);
+    }
+    belong[0] = 1;
+    for (auto p : to[0]) hp.push(p);
+
+    int ret = 0;
+    while (hp.size())
+    {
+        auto [ecost, i] = hp.top();
+        hp.pop();
+        if (belong[i]) continue;
+        ret += ecost;
+        belong[i] = 1;
+        for (auto p : to[i]) if (!belong[p.second]) hp.push(p);
+    }
+    cout << ret << '\n';
+}
