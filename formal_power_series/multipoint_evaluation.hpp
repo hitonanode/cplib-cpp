d6c44baa163a95a63de6c897617af6c8d8bf5aa3
rsm9 <32937551+rsm9@users.noreply.github.com>
2020-05-06 03:46:21 +0900

    multipoint evaluation of polynomial

---
+++ formal_power_series/multipoint_evaluation.hpp
@@ -0,0 +1,48 @@
+#pragma once
+#include "formal_power_series/formal_power_series.hpp"
+#include <vector>
+
+// multipoint polynomial evaluation
+// input: xs = [x_0, ..., x_{N - 1}]: points to evaluate
+//        f = \sum_i^M f_i x^i
+// Complexity: O(N (lgN)^2) building, O(N (lgN)^2 + M lg M) evaluation
+template <typename _Tfield>
+struct ArbitraryModMultipointEvaluation
+{
+    int nx;
+    int head;
+    using polynomial = FormalPowerSeries<_Tfield>;
+    std::vector<polynomial> segtree;
+    ArbitraryModMultipointEvaluation(const std::vector<_Tfield> &xs) : nx(xs.size())
+    {
+        head = 1;
+        while (1 << head < nx) head++;
+        segtree.resize(2 << head);
+        for (int i = 0; i < 1 << head; i++)
+        {
+            segtree[(1 << head) + i] = {i < nx ? -xs[i] : 0, 1};
+        }
+        for (int i = 1 << head; --i;)
+        {
+            segtree[i] = segtree[2 * i] * segtree[2 * i + 1];
+        }
+    }
+    std::vector<_Tfield> ret;
+    void _dfs_eval(polynomial f, int now)
+    {
+        f %= segtree[now];
+        if (now - (1 << head) >= 0)
+        {
+            if ((now - (1 << head)) < nx) ret[now - (1 << head)] = f.coeff(0);
+            return;
+        }
+        _dfs_eval(f, 2 * now);
+        _dfs_eval(f, 2 * now + 1);
+    }
+    std::vector<_Tfield> evaluate_polynomial(polynomial f)
+    {
+        ret.resize(nx);
+        _dfs_eval(f, 1);
+        return ret;
+    }
+};
---
+++ formal_power_series/test/multipoint_evaluation_arbitrary_mod.test.cpp
@@ -0,0 +1,24 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/multipoint_evaluation"
+#include "modulus/modint_fixed.hpp"
+#include "formal_power_series/formal_power_series.hpp"
+#include "formal_power_series/multipoint_evaluation.hpp"
+#include <iostream>
+
+using mint = ModInt<998244353>;
+
+int main()
+{
+    std::cin.tie(NULL);
+    std::ios::sync_with_stdio(false);
+
+    int N, M;
+    std::cin >> N >> M;
+    FormalPowerSeries<ModInt<998244353>> f(N);
+    std::vector<mint> xs(M);
+    for (auto &a : f) std::cin >> a;
+    for (auto &x : xs) std::cin >> x;
+
+    ArbitraryModMultipointEvaluation me(xs);
+    for (auto x : me.evaluate_polynomial(f)) std::cout << x << ' ';
+    std::cout << '\n';
+}
