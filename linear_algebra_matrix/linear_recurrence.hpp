98a1a51de861257fa1c18503705469446a39ec0d
rsm9 <32937551+rsm9@users.noreply.github.com>
2020-05-05 22:31:32 +0900

    kitamasa

--- linear_algebra_matrix/linear_recurrence.hpp
+++ linear_algebra_matrix/linear_recurrence.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <algorithm>
+#include <cassert>
 #include <utility>
 #include <vector>
 
@@ -8,8 +9,9 @@
 // <https://en.wikipedia.org/wiki/Berlekamp%E2%80%93Massey_algorithm>
 // Complexity: O(N^2)
 // input: S = sequence from field K
-// return: (L=degree of minimal polynomial, C=min. poly.(size = L + 1, C[0] = 1))
-// Formula: convolve(S, C)[i] = 0 for i >= L
+// return: L          = degree of minimal polynomial,
+//         C_reversed = monic min. polynomial (size = L + 1, reversed order, C_reversed[0] = 1))
+// Formula: convolve(S, C_reversed)[i] = 0 for i >= L
 // Example:
 // - [1, 2, 4, 8, 16]   -> (1, [1, -2])
 // - [1, 1, 2, 3, 5, 8] -> (2, [1, -1, -1])
@@ -17,37 +19,87 @@
 // - []                 -> (0, [1])
 // - [0, 0, 0]          -> (0, [1])
 // - [-2]               -> (1, [1, 2])
-template <typename K>
-std::pair<int, std::vector<K>> linear_recurrence(const std::vector<K> &S)
+template <typename _Tfield>
+std::pair<int, std::vector<_Tfield>> linear_recurrence(const std::vector<_Tfield> &S)
 {
     int N = S.size();
-    using poly = std::vector<K>;
-    poly C{1}, B{1};
+    using poly = std::vector<_Tfield>;
+    poly C_reversed{1}, B{1};
     int L = 0, m = 1;
-    K b = 1;
+    _Tfield b = 1;
 
     // adjust: C(x) <- C(x) - (d / b) x^m B(x)
-    auto adjust = [](poly C, const poly &B, K d, K b, int m) -> poly {
+    auto adjust = [](poly C, const poly &B, _Tfield d, _Tfield b, int m) -> poly {
         C.resize(std::max(C.size(), B.size() + m));
-        K a = d / b;
+        _Tfield a = d / b;
         for (unsigned i = 0; i < B.size(); i++) C[i + m] -= a * B[i];
         return C;
     };
 
     for (int n = 0; n < N; n++) {
-        K d = S[n];
-        for (int i = 1; i <= L; i++) d += C[i] * S[n - i];
+        _Tfield d = S[n];
+        for (int i = 1; i <= L; i++) d += C_reversed[i] * S[n - i];
 
         if (d == 0) m++;
         else if (2 * L <= n) {
-            poly T = C;
-            C = adjust(C, B, d, b, m);
+            poly T = C_reversed;
+            C_reversed = adjust(C_reversed, B, d, b, m);
             L = n + 1 - L;
             B = T;
             b = d;
             m = 1;
         }
-        else C = adjust(C, B, d, b, m++);
+        else C_reversed = adjust(C_reversed, B, d, b, m++);
     }
-    return std::make_pair(L, C);
+    return std::make_pair(L, C_reversed);
+}
+
+
+// Calculate x^N mod f(x)
+// Known as `Kitamasa method`
+// Input: f_reversed: monic, reversed (f_reversed[0] = 1)
+// Complexity: O(K^2 lgN) (K: deg. of f)
+// Example: (4, [1, -1, -1]) -> [2, 3]
+//          ( x^4 = (x^2 + x + 2)(x^2 - x - 1) + 3x + 2 )
+// Reference: <http://misawa.github.io/others/fast_kitamasa_method.html>
+//            <http://sugarknri.hatenablog.com/entry/2017/11/18/233936>
+template <typename _Tfield>
+std::vector<_Tfield> monomial_mod_polynomial(long long N, const std::vector<_Tfield> &f_reversed)
+{
+    assert(!f_reversed.empty() and f_reversed[0] == 1);
+    int K = f_reversed.size() - 1;
+    if (!K) return {};
+    int D = 64 - __builtin_clzll(N);
+    std::vector<_Tfield> ret(K, 0);
+    ret[0] = 1;
+    auto self_conv = [](std::vector<_Tfield> x) -> std::vector<_Tfield> {
+        int d = x.size();
+        std::vector<_Tfield> ret(d * 2 - 1);
+        for (int i = 0; i < d; i++)
+        {
+            ret[i * 2] += x[i] * x[i];
+            for (int j = 0; j < i; j++) ret[i + j] += x[i] * x[j] * 2;
+        }
+        return ret;
+    };
+    for (int d = D; d--;)
+    {
+        ret = self_conv(ret);
+        for (int i = 2 * K - 2; i >= K; i--)
+        {
+            for (int j = 1; j <= K; j++) ret[i - j] -= ret[i] * f_reversed[j];
+        }
+        ret.resize(K);
+        if ((N >> d) & 1)
+        {
+            std::vector<_Tfield> c(K);
+            c[0] = -ret[K - 1] * f_reversed[K];
+            for (int i = 1; i < K; i++)
+            {
+                c[i] = ret[i - 1] - ret[K - 1] * f_reversed[K - i];
+            }
+            ret = c;
+        }
+    }
+    return ret;
 }
---
+++ linear_algebra_matrix/test/kitamasa.test.cpp
@@ -0,0 +1,78 @@
+#define PROBLEM "https://yukicoder.me/problems/no/214"
+#include "linear_algebra_matrix/linear_recurrence.hpp"
+#include "modulus/modint_fixed.hpp"
+#include "convolution/ntt.hpp"
+using mint = ModInt<1000000007>;
+
+#include <iostream>
+#include <numeric>
+template<typename T> istream &operator>>(istream &is, vector<T> &vec){ for (auto &v : vec) is >> v; return is; }
+#define dbg(x) cerr << #x << " = " << (x) << " (L" << __LINE__ << ") " << __FILE__ << endl;
+
+
+std::vector<mint> gen_dp(std::vector<int> v, int n)
+{
+    vector<vector<mint>> dp(n + 1, vector<mint>(v.back() * n + 1));
+    dp[0][0] = 1;
+    for (auto x : v)
+    {
+        for (int i = n - 1; i >= 0; i--)
+        {
+            for (int j = 0; j < dp[i].size(); j++) if (dp[i][j])
+            {
+                for (int k = 1; i + k <= n; k++) dp[i + k][j + x * k] += dp[i][j];
+            }
+        }
+    }
+    return dp.back();
+}
+
+int main()
+{
+    long long N;
+    int P, C;
+    std::cin >> N >> P >> C;
+    std::vector<mint> primes = gen_dp({2, 3, 5, 7, 11, 13}, P), composites = gen_dp({4, 6, 8, 9, 10, 12}, C);
+    std::vector<mint> f_reversed = nttconv(primes, composites);
+    std::vector<mint> dp(f_reversed.size());
+    dp[0] = 1;
+    for (int i = 0; i < dp.size(); i++)
+    {
+        for (int j = 1; i + j < dp.size(); j++) dp[i + j] += dp[i] * f_reversed[j];
+    }
+
+    for (auto &x : f_reversed) x = -x;
+    f_reversed[0] = 1;
+
+    std::vector<mint> g(f_reversed.size() - 1);
+    g[0] = 1;
+    if (N > f_reversed.size())
+    {
+        long long d = N - f_reversed.size();
+        N -= d;
+        g = monomial_mod_polynomial<mint>(d, f_reversed);
+    }
+
+    auto prod_x = [&](std::vector<mint> v) -> std::vector<mint> {
+        int K = v.size();
+        std::vector<mint> c(K);
+        c[0] = -v[K - 1] * f_reversed[K];
+        for (int i = 1; i < K; i++)
+        {
+            c[i] = v[i - 1] - v[K - 1] * f_reversed[K - i];
+        }
+        return c;
+    };
+    mint acc = 0;
+    for (int i = N; i < f_reversed.size(); i++) acc += f_reversed[i];
+    mint ret = 0;
+    while (N)
+    {
+        mint p = std::inner_product(g.begin(), g.end(), dp.begin(), mint(0));
+        ret -= acc * p;
+        g = prod_x(g);
+        N--;
+        acc += f_reversed[N];
+    }
+    cout << ret << '\n';
+}
