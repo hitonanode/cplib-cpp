7c6c633adf6db46c4a56d092c613f8d553688537
rsm9 <32937551+rsm9@users.noreply.github.com>
2020-09-05 17:47:08 +0900

    #p subset sum

---
+++ formal_power_series/test/sharp_p_subset_sum.test.cpp
@@ -0,0 +1,39 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/sharp_p_subset_sum"
+#include <iostream>
+#include <vector>
+#include "modulus/modint_fixed.hpp"
+#include "formal_power_series/formal_power_series.hpp"
+using namespace std;
+
+int main()
+{
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    int N, T;
+    cin >> N >> T;
+    vector<int> cnt(T + 1);
+    while (N--)
+    {
+        int s;
+        cin >> s;
+        cnt[s]++;
+    }
+    FormalPowerSeries<ModInt<998244353>> A(T + 1), invs(T + 1);
+    for (int i = 1; i <= T; i++) invs[i] = (invs[i] + i).inv();
+
+    for (int i = 1; i <= T; i++)
+    {
+        if (cnt[i])
+        {
+            for (int j = 1; j * i <= T; j++)
+            {
+                if (j % 2) A[j * i] += cnt[i] * invs[j];
+                else A[j * i] -= cnt[i] * invs[j];
+            }
+        }
+    }
+    A = A.exp(T + 1);
+    for (int t = 1; t <= T; t++) cout << A.coeff(t) << ' ';
+    cout << '\n';
+}
