20f255cc98cefe2f71773b82b95f07e91882d621
rsm9 <32937551+rsm9@users.noreply.github.com>
2020-10-12 02:03:05 +0900

    rotate90

---
+++ utilities/rotate90.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#include <vector>
+
+// CUT begin
+template <typename T>
+std::vector<std::vector<T>> rotate90(const std::vector<std::vector<T>>& in)
+{
+    const size_t H = in.size(), W = in[0].size();
+    std::vector<std::vector<T>> ret(W, std::vector<T>(H));
+    for (size_t i = 0; i < H; i++) {
+        for (size_t j = 0; j < W; j++) {
+            ret[j][i] = in[i][W - 1 - j];
+        }
+    }
+    return ret;
+}
