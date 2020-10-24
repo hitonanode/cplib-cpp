03066f5cdc22558cf31feb113ba038ce032bcb81
rsm9 <32937551+rsm9@users.noreply.github.com>
2020-10-24 19:13:38 +0900

    Comment: Aho-Corasick

--- string/aho_corasick.hpp
+++ string/aho_corasick.hpp
@@ -3,18 +3,20 @@
 #include <queue>
 #include <string>
 #include <vector>
-using namespace std;
 
 // CUT begin
-template<typename T>
-struct Trie
-{
-    vector<map<char, int>> child;
-    vector<int> fail;
-    vector<T> node_info;
+// Aho-Corasick algorithm
+// Complexity:
+// - add_keyword(): O(|str|)
+// - build_aho_corasick(): O(\sum_i |str_i|)
+template <typename T>
+struct Trie {
+    std::vector<std::map<char, int>> child;
+    std::vector<int> fail;
+    std::vector<T> node_info;
     int root;
     Trie() : child(1), fail(1), node_info(1), root(0) {}
-    void add_keyword(string str, T info)
+    void add_keyword(const std::string &str, T info)
     {
         int now = 0;
         for (auto c : str)
@@ -22,7 +24,7 @@ struct Trie
             if (child[now].count(c) == 0)
             {
                 child[now][c] = fail.size();
-                child.emplace_back(map<char, int>());
+                child.emplace_back();
                 fail.emplace_back(root);
                 node_info.emplace_back(0);
             }
@@ -33,7 +35,7 @@ struct Trie
 
     void build_aho_corasick()
     {
-        queue<int> q;
+        std::queue<int> q;
         for (auto pa : child[root]) q.push(pa.second);
         while (!q.empty())
         {
