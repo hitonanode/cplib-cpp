89d59e69ec97251053b4cfa686d2d4ba0400f1b5
rsm9 <32937551+rsm9@users.noreply.github.com>
2020-11-18 23:19:38 +0900

    ACL lazy segtree

---
+++ segmenttree/acl_range-update-range-sum-min.hpp
@@ -0,0 +1,24 @@
+#pragma once
+#include <algorithm>
+
+// CUT begin
+#include "acl_lazysegtree.hpp"
+namespace rangeupdaterangesummin {
+using Int = long long;
+const Int INF = 101010101010LL;
+struct S {
+    Int sum;
+    int sz;
+    Int minval;
+};
+using F = std::pair<bool, Int>;
+S op(S a, S b) { return S{a.sum + b.sum, a.sz + b.sz, std::min(a.minval, b.minval)}; }
+S mapping(F f, S x) { return f.first ? S{x.sz * f.second, x.sz, f.second} : x; }
+F composition(F later, F conventional) { return later.first ? later : conventional; }
+S e() { return S{0, 0, INF}; }
+F id() { return std::make_pair(false, INF); }
+using RangeUpdateRangeSumMin = atcoder::lazy_segtree<S, op, e, F, mapping, composition, id>;
+}; // namespace rangeupdaterangesummin
+using rangeupdaterangesummin::RangeUpdateRangeSumMin;
+using rangeupdaterangesummin::S;
+// Example of usage <https://codeforces.com/contest/1439/submission/98816580>
---
+++ segmenttree/test/acl_range-affine-range-sum.test.cpp
@@ -0,0 +1,26 @@
+#include "../acl_range-affine-range-sum.hpp"
+#include "modint.hpp"
+#define PROBLEM "https://judge.yosupo.jp/problem/range_affine_range_sum"
+using namespace std;
+
+int main() {
+    cin.tie(nullptr), ios::sync_with_stdio(false);
+
+    int N, Q;
+    cin >> N >> Q;
+
+    vector<S> A(N);
+    for (auto &a : A) cin >> a.first, a.second = 1;
+    RangeAffineRangeSum segtree(A);
+    while (Q--) {
+        int q, l, r;
+        cin >> q >> l >> r;
+        if (q) {
+            cout << segtree.prod(l, r).first << '\n';
+        } else {
+            F f;
+            cin >> f.first >> f.second;
+            segtree.apply(l, r, f);
+        }
+    }
+}
