67665e6600d2547e6feeef20bbc2def42615c528
rsm9 <32937551+rsm9@users.noreply.github.com>
2020-11-18 23:31:57 +0900

    Test for Sparse Table (aoj) trailing spaces trimmed

--- sparse_table/test/sparse_table_aoj.test.cpp
+++ sparse_table/test/sparse_table_aoj.test.cpp
@@ -1,20 +1,24 @@
 #include "sparse_table/rmq_sparse_table.hpp"
 #include "sparse_table/sparse_table.hpp"
+#include <iostream>
+#include <vector>
 #define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_3_D"
+using namespace std;
 
-#include <cstdio>
 int main() {
+    cin.tie(nullptr), ios::sync_with_stdio(false);
+
     int N, L;
-    scanf("%d %d", &N, &L);
-    std::vector<int> A(N);
-    for (auto &a : A) scanf("%d", &a);
-    auto f = [](int l, int r) { return std::min(l, r); };
+    cin >> N >> L;
+    vector<int> A(N);
+    for (auto &a : A) cin >> a;
+    auto f = [](int l, int r) { return min(l, r); };
     SparseTable<int, decltype(f)> sp1(A, 1e9, f);
     StaticRMQ<int> sp2(A, 1e9);
     for (int l = 0; l + L <= N; l++) {
         int a = sp1.get(l, l + L);
         int b = sp2.get(l, l + L);
         assert(a == b);
-        printf("%d ", a);
+        cout << a << (l + L == N ? '\n' : ' ');
     }
 }
