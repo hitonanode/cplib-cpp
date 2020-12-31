21282238451396d29de2b4392d3fea0edebbc198
hitonanode <32937551+hitonanode@users.noreply.github.com>
2020-12-31 18:25:06 +0900

    Added Aho-Corasick tests

---
+++ string/test/aho_corasick_unorderedmap.test.cpp
@@ -0,0 +1,16 @@
+#include "../aho_corasick.hpp"
+#include <iostream>
+using namespace std;
+#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ALDS1_14_D"
+
+int main() {
+    cin.tie(nullptr), ios::sync_with_stdio(false);
+    AhoCorasick<TrieNodeUM, c2i0aA> trie(62);
+    string T, P;
+    int Q;
+    cin >> T >> Q;
+    while (Q--) cin >> P, trie.add(P);
+
+    trie.build_aho_corasick();
+    for (auto n : trie.match(T)) cout << !!n << '\n';
+}
