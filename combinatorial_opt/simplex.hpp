46fe0bbfbca6b21a36522bf89bf341c39dda745b
hitonanode <32937551+hitonanode@users.noreply.github.com>
2021-02-11 23:53:14 +0900

    Simplex: Unify notation between code and markdown

--- combinatorial_opt/simplex.hpp
+++ combinatorial_opt/simplex.hpp
@@ -11,21 +11,21 @@ template <typename Float = double, int DEPS = 30> struct Simplex {
     const Float EPS = Float(1.0) / (1LL << DEPS);
     int N, M;
     std::vector<int> idx;
-    std::vector<std::vector<Float>> a;
+    std::vector<std::vector<Float>> mat;
     int i_ch, j_ch;
 
     void initialize(const std::vector<std::vector<Float>> &A, const std::vector<Float> &b, const std::vector<Float> &c) {
         N = c.size(), M = A.size();
 
-        a.assign(M + 2, std::vector<Float>(N + 2));
+        mat.assign(M + 2, std::vector<Float>(N + 2));
         i_ch = M;
         for (int i = 0; i < M; i++) {
-            for (int j = 0; j < N; j++) a[i][j] = -A[i][j];
-            a[i][N] = 1, a[i][N + 1] = b[i];
-            if (a[i_ch][N + 1] > a[i][N + 1]) i_ch = i;
+            for (int j = 0; j < N; j++) mat[i][j] = -A[i][j];
+            mat[i][N] = 1, mat[i][N + 1] = b[i];
+            if (mat[i_ch][N + 1] > mat[i][N + 1]) i_ch = i;
         }
-        for (int j = 0; j < N; j++) a[M][j] = c[j];
-        a[M + 1][N] = -1;
+        for (int j = 0; j < N; j++) mat[M][j] = c[j];
+        mat[M + 1][N] = -1;
 
         idx.resize(N + M + 1);
         std::iota(idx.begin(), idx.end(), 0);
@@ -37,37 +37,37 @@ template <typename Float = double, int DEPS = 30> struct Simplex {
         for (j_ch = N;;) {
             if (i_ch < M) {
                 std::swap(idx[j_ch], idx[i_ch + N + 1]);
-                a[i_ch][j_ch] = Float(1) / a[i_ch][j_ch];
+                mat[i_ch][j_ch] = Float(1) / mat[i_ch][j_ch];
                 jupd.clear();
                 for (int j = 0; j < N + 2; j++) {
                     if (j != j_ch) {
-                        a[i_ch][j] *= -a[i_ch][j_ch];
-                        if (abs_(a[i_ch][j]) > EPS) jupd.push_back(j);
+                        mat[i_ch][j] *= -mat[i_ch][j_ch];
+                        if (abs_(mat[i_ch][j]) > EPS) jupd.push_back(j);
                     }
                 }
                 for (int i = 0; i < M + 2; i++) {
-                    if (abs_(a[i][j_ch]) < EPS or i == i_ch) continue;
-                    for (auto j : jupd) a[i][j] += a[i][j_ch] * a[i_ch][j];
-                    a[i][j_ch] *= a[i_ch][j_ch];
+                    if (abs_(mat[i][j_ch]) < EPS or i == i_ch) continue;
+                    for (auto j : jupd) mat[i][j] += mat[i][j_ch] * mat[i_ch][j];
+                    mat[i][j_ch] *= mat[i_ch][j_ch];
                 }
             }
 
             j_ch = -1;
             for (int j = 0; j < N + 1; j++) {
                 if (j_ch < 0 or idx[j_ch] > idx[j]) {
-                    if (a[M + 1][j] > EPS or (abs_(a[M + 1][j]) < EPS and a[M][j] > EPS)) j_ch = j;
+                    if (mat[M + 1][j] > EPS or (abs_(mat[M + 1][j]) < EPS and mat[M][j] > EPS)) j_ch = j;
                 }
             }
             if (j_ch < 0) break;
 
             i_ch = -1;
             for (int i = 0; i < M; i++) {
-                if (a[i][j_ch] < -EPS) {
+                if (mat[i][j_ch] < -EPS) {
                     if (i_ch < 0) {
                         i_ch = i;
-                    } else if (a[i_ch][N + 1] / a[i_ch][j_ch] - a[i][N + 1] / a[i][j_ch] < -EPS) {
+                    } else if (mat[i_ch][N + 1] / mat[i_ch][j_ch] - mat[i][N + 1] / mat[i][j_ch] < -EPS) {
                         i_ch = i;
-                    } else if (a[i_ch][N + 1] / a[i_ch][j_ch] - a[i][N + 1] / a[i][j_ch] < EPS and idx[i_ch] > idx[i]) {
+                    } else if (mat[i_ch][N + 1] / mat[i_ch][j_ch] - mat[i][N + 1] / mat[i][j_ch] < EPS and idx[i_ch] > idx[i]) {
                         i_ch = i;
                     }
                 }
@@ -77,15 +77,15 @@ template <typename Float = double, int DEPS = 30> struct Simplex {
                 break;
             }
         }
-        if (a[M + 1][N + 1] < -EPS) {
+        if (mat[M + 1][N + 1] < -EPS) {
             infeasible = true;
             return;
         }
         x.assign(N, 0);
         for (int i = 0; i < M; i++) {
-            if (idx[N + 1 + i] < N) x[idx[N + 1 + i]] = a[i][N + 1];
+            if (idx[N + 1 + i] < N) x[idx[N + 1 + i]] = mat[i][N + 1];
         }
-        ans = a[M][N + 1];
+        ans = mat[M][N + 1];
     }
     Simplex(const std::vector<std::vector<Float>> &A, const std::vector<Float> &b, const std::vector<Float> &c) {
         is_infty = infeasible = false;
