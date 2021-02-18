0ca3370c1d59da68e4307a4ca3a52b5bb9474be6
hitonanode <32937551+hitonanode@users.noreply.github.com>
2021-02-19 02:12:16 +0900

    Snippet generator for snippet-from-file

--- generate_snippet_conf.py
+++ generate_snippet_conf.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
-# "Config.txt" generator for "Clipboard History", snippet manager
+# Generate configuration files for snippet managers
+#
+# "ClipBoardHistoryConfig.txt" for "Clipboard History" (Windows)
 # https://blank-note.sakura.ne.jp/topics/clipboard_history.html
+#
+# "SnippetConfig.json" for "snippet-from-file" (VS Code)
+# https://marketplace.visualstudio.com/items?itemName=morioprog.snippet-from-file
 
+import json
 import os
 import re
 
@@ -12,6 +18,7 @@ config_header = [
 
 
 def read_hpp(filepath):
+    print(filepath)
     ret = [os.path.basename(filepath) + '|']
     with open(filepath, 'r') as file:
         for line in file.read().splitlines():
@@ -28,21 +35,29 @@ def read_hpp(filepath):
 
 def dfs(current_dir):
     ret = list()
+    json_ret = dict()
     for ch in os.listdir(current_dir):
         filepath = os.path.join(current_dir, ch)
         if os.path.isdir(filepath) and filepath[2] != '.':
-            ch_info = dfs(filepath)
+            ch_info, json_info = dfs(filepath)
+            json_ret.update(json_info)
             if ch_info:
                 ret += ['+' + ch]
                 ret += ch_info
                 ret += ['..']
         if os.path.isfile(filepath) and len(filepath) > 4 and filepath[-4:] == '.hpp':
             ret += read_hpp(filepath)
-    return ret
+            json_ret[filepath[2:-4]] = filepath
+    return ret, json_ret
 
 
 if __name__ == '__main__':
-    ret = config_header + dfs(rootdir)
+    ret, json_ret = dfs(rootdir)
+    ret = config_header + ret
+
     with open('ClipBoardHistoryConfig.txt', 'w') as f:
         for line in ret:
             f.write(line + '\r\n')
+
+    with open('SnippetConfig.json', 'w') as f:
+        json.dump(json_ret, f, ensure_ascii=False, sort_keys=True, indent=4)
