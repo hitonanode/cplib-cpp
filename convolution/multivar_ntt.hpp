55a10c7a685ad880a29fab28686ec2ec7f084e7c
hitonanode <32937551+hitonanode@users.noreply.github.com>
2021-03-05 02:54:52 +0900

    Add Multivariate Linear Convolution

---
+++ convolution/multivar_ntt.hpp
@@ -0,0 +1,64 @@
+#pragma once
+#include "ntt.hpp"
+#include <cassert>
+#include <numeric>
+#include <vector>
+
+// Multivariate convolution (Linear, overflow cutoff)
+// Complexity: $O(kN \log N + k^2 N)$
+// Note that the vectors store the infomation in **column-major order**
+// Implementation idea: https://rushcheyo.blog.uoj.ac/blog/6547W
+template <typename MODINT> struct multivar_ntt {
+    int K, N, fftlen;
+    std::vector<int> dim;
+    std::vector<int> chi;
+    MODINT invfftlen;
+
+private:
+    void _initialize(const std::vector<int> &dim_) {
+        dim = dim_;
+        K = dim_.size();
+        N = std::accumulate(dim_.begin(), dim_.end(), 1, [&](int l, int r) { return l * r; });
+        fftlen = 1;
+        while (fftlen < N * 2) fftlen <<= 1;
+        invfftlen = MODINT(fftlen).inv();
+
+        chi.resize(fftlen);
+        int t = 1;
+        for (auto d : dim_) {
+            t *= d;
+            for (int s = t; s < fftlen; s += t) chi[s] += 1;
+        }
+        for (int i = 0; i + 1 < fftlen; i++) {
+            chi[i + 1] += chi[i];
+            if (chi[i + 1] >= K) chi[i + 1] -= K;
+        }
+    }
+
+    std::vector<MODINT> _convolve(const std::vector<MODINT> &f, const std::vector<MODINT> &g) {
+        assert(int(f.size()) == N);
+        assert(int(g.size()) == N);
+        if (dim.empty()) return {f[0] * g[0]};
+        std::vector<std::vector<MODINT>> fex(K, std::vector<MODINT>(fftlen)), gex(K, std::vector<MODINT>(fftlen));
+        for (int i = 0; i < N; i++) fex[chi[i]][i] = f[i], gex[chi[i]][i] = g[i];
+        for (auto &vec : fex) ntt(vec, false);
+        for (auto &vec : gex) ntt(vec, false);
+        std::vector<std::vector<MODINT>> hex(K, std::vector<MODINT>(fftlen));
+        for (int df = 0; df < K; df++) {
+            for (int dg = 0; dg < K; dg++) {
+                int dh = (df + dg < K) ? df + dg : df + dg - K;
+                for (int i = 0; i < fftlen; i++) hex[dh][i] += fex[df][i] * gex[dg][i];
+            }
+        }
+        for (auto &vec : hex) ntt(vec, true);
+        std::vector<MODINT> ret(N);
+        for (int i = 0; i < N; i++) ret[i] = hex[chi[i]][i];
+        return ret;
+    }
+
+public:
+    multivar_ntt(const std::vector<int> &dim_) { _initialize(dim_); }
+    std::vector<MODINT> operator()(const std::vector<MODINT> &f, const std::vector<MODINT> &g) {
+        return _convolve(f, g);
+    }
+};
---
+++ convolution/multivar_ntt.md
@@ -0,0 +1,27 @@
+---
+title: Multivariate linear convolution （多変数線形畳み込み）
+documentation_of: ./multivar_ntt.hpp
+---
+
+## 解いてくれる問題
+
+[Library Checker: Multivariate Convolution](https://judge.yosupo.jp/problem/multivariate_convolution)
+
+$\displaystyle
+f(x_1, x_2, \dots, x_K), \ g(x_1, x_2, \dots, x_K)
+$
+
+に対して，
+
+$\displaystyle
+f \cdot g \bmod (x_1^{N_1} x_2^{N_2} \dots x_K^{N_K})
+$
+
+を計算（線形畳み込み，「はみ出し」分は無視）．
+
+### アルゴリズム（要点）
+
+[参考](https://rushcheyo.blog.uoj.ac/blog/6547)
+
+- subset convolution と同様のアイデアを適用する．
+- subset convolution は `popcount` に着目して要素ごとの格納する位置を決めたが，代わりに上のリンクにある $\chi$ 関数を使って決めてあげると万事うまくいく．
