63efae963b35eefae378d0c55731ab9f9721fc8e
hitonanode <32937551+hitonanode@users.noreply.github.com>
2021-04-18 20:20:24 +0900

    Simple rmq: add concise documentation

--- segmenttree/range_minimum_query.hpp
+++ segmenttree/range_minimum_query.hpp
@@ -1,44 +1,46 @@
 #pragma once
 #include <iostream>
+#include <limits>
 #include <vector>
-using namespace std;
 
 // CUT begin
-using T = int;
-struct RangeMinimumQuery {
+// Range Minimum Query
+// - RangeMinimumQuery<T, defaultT>(int N = 0) : Initialize array x = [defaultT, ..., defaultT] of length N, O(N)
+// - RangeMinimumQuery<T, defaultT>(const std::vector<T> &vals) : Initialize array x by [vals[0], ..., vals[-1]], O(N)
+// - update(int pos, T val) : x[pos] <- val, O(log N)
+// - get(int begin, int end) : Get min(x_begin, ..., x_(end - 1)), O(log N)
+template <typename T, T defaultT = std::numeric_limits<T>::max() / 2> struct RangeMinimumQuery {
     int N;
-    T defaultT = 1e9;
     int head;
-    vector<T> x;
+    std::vector<T> x;
     T _get(int begin, int end, int pos, int l, int r) const {
         if (r <= begin or l >= end) return defaultT;
         if (l >= begin and r <= end) return x[pos];
-        return min(_get(begin, end, 2 * pos + 1, l, (l + r) / 2), _get(begin, end, 2 * pos + 2, (l + r) / 2, r));
+        return std::min(_get(begin, end, 2 * pos + 1, l, (l + r) / 2), _get(begin, end, 2 * pos + 2, (l + r) / 2, r));
     }
-    RangeMinimumQuery(int N, T defaultT) : N(N), defaultT(defaultT) {
+    RangeMinimumQuery(int N = 0) : N(N) {
         int N_tmp = 1;
         while (N_tmp < N) N_tmp <<= 1;
         x.assign(N_tmp * 2, defaultT), head = N_tmp - 1;
     }
-    void build(const vector<T> &vals) {
-        copy(vals.begin(), vals.end(), x.begin() + head);
-        for (int i = head - 1; i >= 0; i--) { x[i] = min(x[i * 2 + 1], x[i * 2 + 2]); }
+    void _build(const std::vector<T> &vals) {
+        std::copy(vals.begin(), vals.end(), x.begin() + head);
+        for (int i = head - 1; i >= 0; i--) x[i] = std::min(x[i * 2 + 1], x[i * 2 + 2]);
     }
-    RangeMinimumQuery(const vector<T> &vals, T defaultT) : N(vals.size()), defaultT(defaultT) {
+    RangeMinimumQuery(const std::vector<T> &vals) : N(vals.size()) {
         int N_tmp = 1;
         while (N_tmp < N) N_tmp <<= 1;
         x.assign(N_tmp * 2, defaultT), head = N_tmp - 1;
-        build(vals);
+        _build(vals);
     }
-    RangeMinimumQuery() : RangeMinimumQuery(0, T()) {}
     void update(int pos, T val) {
         pos += head, x[pos] = val;
-        while (pos) pos = (pos - 1) / 2, x[pos] = min(x[pos * 2 + 1], x[pos * 2 + 2]);
+        while (pos) pos = (pos - 1) / 2, x[pos] = std::min(x[pos * 2 + 1], x[pos * 2 + 2]);
     }
     T get(int begin, int end) const { return _get(begin, end, 0, 0, (int)x.size() / 2); }
-    friend ostream &operator<<(ostream &os, const RangeMinimumQuery &s) {
-        os << "[";
-        for (int i = 0; i < s.N; i++) os << s.get(i, i + 1) << ",";
-        return os << "]";
+    friend std::ostream &operator<<(std::ostream &os, const RangeMinimumQuery &s) {
+        os << '[';
+        for (int i = 0; i < s.N; i++) os << s.get(i, i + 1) << ',';
+        return os << ']';
     }
 };
--- segmenttree/test/point-update-range-min.test.cpp
+++ segmenttree/test/point-update-range-min.test.cpp
@@ -6,15 +6,14 @@ using namespace std;
 int main() {
     int N, Q;
     cin >> N >> Q;
-    vector<int> A(N, (1LL << 31) - 1);
-    RangeMinimumQuery rmq(A, A[0]);
+    RangeMinimumQuery<int, (1LL << 31) - 1> rmq(N);
     for (int q = 0; q < Q; q++) {
         int c, x, y;
         cin >> c >> x >> y;
         if (c == 0) {
             rmq.update(x, y);
         } else {
-            cout << rmq.get(x, y + 1) << endl;
+            cout << rmq.get(x, y + 1) << '\n';
         }
     }
 }
