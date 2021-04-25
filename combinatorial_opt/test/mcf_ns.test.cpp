f9278982bf46016bfc41932cc7fcf6bef15bf283
hitonanode <32937551+hitonanode@users.noreply.github.com>
2021-04-25 18:33:22 +0900

    add mincostflow verification for network simplex

---
+++ combinatorial_opt/test/mcf_ns.test.cpp
@@ -0,0 +1,19 @@
+#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_6_B"
+#include "../networksimplex.hpp"
+#include <iostream>
+using namespace std;
+
+int main() {
+    int V, E, F;
+    cin >> V >> E >> F;
+    mcf_graph_ns<int, int> mcf(V);
+    while (E--) {
+        int u, v, c, d;
+        cin >> u >> v >> c >> d;
+        mcf.add_edge(u, v, 0, c, d);
+    }
+
+    mcf.set_supply(0, F), mcf.set_supply(V - 1, -F);
+    auto ret = mcf.solve<int>();
+    cout << (mcf.infeasible ? -1 : ret) << '\n';
+}
