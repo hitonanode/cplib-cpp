9088dad6025b2ea60f3adaee096128d351702eec
hitonanode <32937551+hitonanode@users.noreply.github.com>
2021-05-01 11:39:46 +0900

    Moebius transform for sparse function

--- convolution/convolutive_translation_2d.hpp
+++ convolution/convolutive_translation_2d.hpp
@@ -1,3 +1,4 @@
+#pragma once
 #include "convolution/ntt.hpp"
 #include <algorithm>
 #include <utility>
@@ -8,7 +9,7 @@
 // Verification: TCO 2020 Round 3A 1000 ZombieRPGDice
 // Verification: TCO 2020 Round 3B 500 ShortBugPaths
 template <typename MODINT>
-std::vector<std::vector<MODINT>> convolutive_translation_2d(const std::vector<std::vector<MODINT>> &initial_dist,                    // size: H * W
+std::vector<std::vector<MODINT>> convolutive_translation_2d(const std::vector<std::vector<MODINT>> &initial_dist, // size: H * W
                                                             const std::vector<std::pair<std::pair<int, int>, MODINT>> &trans_coeffs, // [((dx, dy), coefficient), ...]
                                                             int nb_step = 1) {
     int H = initial_dist.size(), W = initial_dist[0].size();
@@ -37,7 +38,9 @@ std::vector<std::vector<MODINT>> convolutive_translation_2d(const std::vector<st
     for (auto p : t2c_1d) { trans1d[tlo + p.first.first * WW + p.first.second] += p.second; }
     for (int t = 0; t < nb_step; t++) {
         auto dp_nxt = nttconv(dp, trans1d);
-        for (int i = 0; i < H; i++) { std::copy(dp_nxt.begin() + i * WW + tlo, dp_nxt.begin() + i * WW + W + tlo, dp.begin() + i * WW); }
+        for (int i = 0; i < H; i++) {
+            std::copy(dp_nxt.begin() + i * WW + tlo, dp_nxt.begin() + i * WW + W + tlo, dp.begin() + i * WW);
+        }
     }
     std::vector<std::vector<MODINT>> ret(H);
     for (int i = 0; i < H; i++) { ret[i].insert(ret[i].end(), dp.begin() + i * WW, dp.begin() + i * WW + W); }
