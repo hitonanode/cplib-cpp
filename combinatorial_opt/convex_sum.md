19ba868cbfaaa4e2ba28d7d51d0a0d3684863aba
Ryotaro Sato <32937551+hitonanode@users.noreply.github.com>
2021-05-11 22:29:33 +0900

    Convex sum (#27)
    * convex sum
    
    * [auto-verifier] verify commit f69960704907d9252428550f6324aefeff1f113b
    
    Co-authored-by: GitHub <noreply@github.com>

---
+++ combinatorial_opt/convex_sum.md
@@ -0,0 +1,17 @@
+---
+title: 引数総和に関する等式制約下のもとでの互いに独立な離散凸関数総和の最小化
+documentation_of: ./convex_sum.hpp
+---
+
+コスト関数
+
+$\displaystyle
+y = \sum_{i=1}^N \sum_{j=1}^{k_i} f_i (x_{ij})
+$
+
+を，等式制約 $\displaystyle \sum_{i=1}^N \sum_{j=1}^{k_i} x_{ij} = C$ のもと最小化する．
+
+## 問題例
+
+- [Codeforces Round #639 (Div. 1) D. Résumé Review](https://codeforces.com/contest/1344/problem/D)
+- [yukicoder No.1495 パンの仕入れ](https://yukicoder.me/problems/no/1495)
---
+++ combinatorial_opt/test/convex_sum.test.cpp
@@ -0,0 +1,32 @@
+#include "../convex_sum.hpp"
+#include <cassert>
+#include <iostream>
+#define PROBLEM "https://yukicoder.me/problems/no/1495"
+using namespace std;
+
+long long solve() {
+    using lint = long long;
+    int N, M, K;
+    cin >> N >> M >> K;
+    vector<lint> a(N), b(N), c(N);
+    while (M--) {
+        int x;
+        lint y;
+        cin >> x >> y;
+        x--;
+        a[x]++;
+        b[x] -= y * 2;
+        c[x] += y * y;
+    }
+    vector<Quadratic> costs;
+    for (int i = 0; i < N; i++) costs.emplace_back(a[i], b[i], c[i], 0, K);
+    auto sol = MinConvexSumUnderLinearConstraint<Quadratic, lint, 1LL << 60>(vector<lint>(N, 1), costs, K);
+    return sol.first;
+}
+
+int main() {
+    cin.tie(nullptr), ios::sync_with_stdio(false);
+    int T;
+    cin >> T;
+    while (T--) cout << solve() << '\n';
+}
