befed8eff2d80ce62ddc8458eda3bb68b5bbdbd9
hitonanode <32937551+hitonanode@users.noreply.github.com>
2021-06-06 16:39:38 +0900

    Add Miller-Rabin is_prime() test up to 2e8

---
+++ number/test/miller-rabin-2e8.test.cpp
@@ -0,0 +1,16 @@
+#define PROBLEM "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ITP1_1_A" // DUMMY
+#include "../factorize.hpp"
+#include "../sieve.hpp"
+#include <cassert>
+#include <cstdio>
+
+int main() {
+    int lim = 2e8;
+    Sieve sieve(lim);
+    for (int x = 1; x <= lim; x++) {
+        bool is_prime_1 = (sieve.min_factor[x] == x);
+        bool is_prime_2 = is_prime(x);
+        assert(is_prime_1 == is_prime_2);
+    }
+    puts("Hello World");
+}
