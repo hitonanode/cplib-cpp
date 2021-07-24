39e56a39b34b45fe9cf1373675642231ff8d95c1
hitonanode <32937551+hitonanode@users.noreply.github.com>
2021-07-25 00:22:37 +0900

    Update floor_sum

--- utilities/floor_sum.hpp
+++ utilities/floor_sum.hpp
@@ -1,25 +1,51 @@
 #pragma once
+#include <utility>
 
 // CUT begin
 // \sum_{i=0}^{n-1} floor((ai + b) / m)
-// 0 <= n <= 1e9
-// 1 <= m <= 1e9
+// 0 <= n < 2e32
+// 1 <= m < 2e32
 // 0 <= a, b < m
-// Complexity: O(lg(n + m + a + b))
+// Complexity: O(lg(m))
 long long floor_sum(long long n, long long m, long long a, long long b) {
-    long long ans = 0;
-    if (a >= m) {
-        ans += (n - 1) * n * (a / m) / 2;
-        a %= m;
-    }
-    if (b >= m) {
-        ans += n * (b / m);
-        b %= m;
-    }
+    auto safe_mod = [](long long x, long long m) -> long long {
+        x %= m;
+        if (x < 0) x += m;
+        return x;
+    };
+    auto floor_sum_unsigned = [](unsigned long long n, unsigned long long m, unsigned long long a, unsigned long long b) -> unsigned long long {
+        unsigned long long ans = 0;
+        while (true) {
+            if (a >= m) {
+                ans += n * (n - 1) / 2 * (a / m);
+                a %= m;
+            }
+            if (b >= m) {
+                ans += n * (b / m);
+                b %= m;
+            }
 
-    long long y_max = (a * n + b) / m, x_max = (y_max * m - b);
-    if (y_max == 0) return ans;
-    ans += (n - (x_max + a - 1) / a) * y_max;
-    ans += floor_sum(y_max, a, m, (a - x_max % a) % a);
-    return ans;
+            unsigned long long y_max = a * n + b;
+            if (y_max < m) break;
+            // y_max < m * (n + 1)
+            // floor(y_max / m) <= n
+            n = (unsigned long long)(y_max / m);
+            b = (unsigned long long)(y_max % m);
+            std::swap(m, a);
+        }
+        return ans;
+    };
+
+    unsigned long long ans = 0;
+    if (a < 0) {
+        unsigned long long a2 = safe_mod(a, m);
+        ans -= 1ULL * n * (n - 1) / 2 * ((a2 - a) / m);
+        a = a2;
+    }
+    if (b < 0) {
+        unsigned long long b2 = safe_mod(b, m);
+        ans -= 1ULL * n * ((b2 - b) / m);
+        b = b2;
+    }
+    return ans + floor_sum_unsigned(n, m, a, b);
 }
