8fa528d014d6cc78b63cb0013ed6aeea839c89a5
hitonanode <32937551+hitonanode@users.noreply.github.com>
2021-08-23 00:03:07 +0900

    Offline sum of linear

---
+++ utilities/offline_sum_of_linear.hpp
@@ -0,0 +1,43 @@
+#pragma once
+#include <cassert>
+#include <tuple>
+#include <vector>
+
+// CUT begin
+// Offline sum of linear segments
+template <class T> struct offline_sum_of_linear {
+    int N;
+    std::vector<T> ret;
+    std::vector<std::tuple<int, int, T, T>> upds;
+    offline_sum_of_linear(int N_) : N(N_), ret(N_) {}
+
+    // Add f0 + df * (x - l) to A[x] for x in [l, r)
+    void add_linear(int l, int r, T f0, T df) {
+        assert(0 <= l and l <= r and r <= N);
+        if (l < r) upds.emplace_back(l, r, f0, df);
+    }
+    void run() {
+        if (upds.empty()) return;
+        std::vector<T> tmp(N + 1);
+        int l, r;
+        T f0, df;
+        for (const auto &q : upds) {
+            std::tie(l, r, f0, df) = q;
+            tmp[l + 1] += df;
+            tmp[r] -= df;
+        }
+        for (int i = 1; i < N; i++) tmp[i] += tmp[i - 1];
+        for (const auto &q : upds) {
+            std::tie(l, r, f0, df) = q;
+            tmp[l] += f0;
+            tmp[r] -= f0 + (r - l - 1) * df;
+        }
+        upds.clear();
+        for (int i = 1; i < N; i++) tmp[i] += tmp[i - 1], ret[i] += tmp[i];
+    }
+    T operator[](int i) {
+        assert(0 <= i and i < N);
+        return ret[i];
+    }
+    std::vector<T> vals() { return run(), ret; }
+};
---
+++ utilities/offline_sum_of_linear.md
@@ -0,0 +1,23 @@
+---
+title: Offline sum of linear segments （区間一次関数加算クエリの累積和を用いたオフライン処理）
+documentation_of: ./offline_sum_of_linear.hpp
+---
+
+全ての項の値がゼロで初期化された数列 $[A_0, \dots, A_{N -1}]$ に対して以下のクエリを処理．
+
+- $l \le i < r$ を満たす各 $i$ について $A_i$ に $f_0 + (i - l) \cdot df$ を加算．時間計算量 $O(1)$．
+- $[A_0, \dots, A_{N - 1}]$ の値を陽に計算・取得．$O(N)$．
+
+## 使用方法
+
+```cpp
+offline_sum_of_linear<long long> f(N);
+f.add_linear(l, r, f0, df);
+
+long long vi = f[i]; // 特定の項の取得
+vector<long long> values = f.vals(); // [f[0], ..., f[N - 1]] の一括取得
+```
+
+## 問題例
+
+- [AtCoder Regular Contest 125 E - Snack](https://atcoder.jp/contests/arc125/tasks/arc125_e)
