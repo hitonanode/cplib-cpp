5cfb73efa5bb6e082e6997a578fb94f45b15cfc6
hitonanode <32937551+hitonanode@users.noreply.github.com>
2021-08-26 22:06:32 +0900

    Use fast simpler pqueue

--- combinatorial_opt/maxflow_pushrelabel.hpp
+++ combinatorial_opt/maxflow_pushrelabel.hpp
@@ -8,6 +8,21 @@
 // Complexity: O(N^2 M^(1/2))
 template <class Cap, Cap INF = std::numeric_limits<Cap>::max() / 2, bool UseGlobalRelabeling = false>
 struct mf_pushrelabel {
+    struct pque_ {
+        std::vector<std::pair<int, int>> even_, odd_;
+        int se, so;
+        void init(int n) { even_.resize(n), odd_.resize(n), se = so = 0; };
+        void clear() { se = so = 0; }
+        bool empty() const { return se + so == 0; }
+        void push(int i, int h) { (h & 1 ? odd_[so++] : even_[se++]) = {i, h}; }
+        int pop() {
+            if (!se or (so and odd_[so - 1].second > even_[se - 1].second)) {
+                return odd_[--so].first;
+            } else {
+                return even_[--se].first;
+            }
+        }
+    } pque;
     int _n;
     struct _edge {
         int to, rev;
@@ -30,23 +45,16 @@ struct mf_pushrelabel {
 
     std::vector<int> dist;
     std::vector<Cap> excess;
-    std::vector<std::vector<int>> h2v;
-    int max_height;
-    void activate(int i) {
-        h2v[dist[i]].push_back(i);
-        if (dist[i] > max_height) max_height = dist[i];
-    }
     void global_relabeling(int t) {
         if (!UseGlobalRelabeling) return;
         dist.assign(_n, _n), dist[t] = 0;
         static std::vector<int> q;
         q = {t};
         int qh = 0;
-        h2v.assign(_n * 2, {});
-        max_height = -1;
+        pque.clear();
         while (qh < int(q.size())) {
             int now = q[qh++];
-            if (excess[now] > 0) activate(now);
+            if (excess[now] > 0) pque.push(now, dist[now]);
             for (const auto &e : g[now]) {
                 if (g[e.to][e.rev].cap and dist[e.to] == _n) {
                     dist[e.to] = dist[now] + 1;
@@ -63,18 +71,12 @@ struct mf_pushrelabel {
         excess[s] = INF, excess[t] = -INF;
         dist.assign(_n, 0);
         dist[s] = _n;
-        h2v.assign(_n * 2, {});
-        max_height = -1;
+        pque.init(_n);
         for (auto &e : g[s]) push(s, e);
         global_relabeling(t);
         int tick = _n;
-        while (max_height >= 0) {
-            if (h2v[max_height].empty()) {
-                max_height--;
-                continue;
-            }
-            int i = h2v[max_height].back();
-            h2v[max_height].pop_back();
+        while (!pque.empty()) {
+            int i = pque.pop();
             int dnxt = _n * 2 - 1;
             for (auto &e : g[i]) {
                 if (!e.cap) continue;
@@ -85,10 +87,7 @@ struct mf_pushrelabel {
                     if (dist[e.to] + 1 < dnxt) dnxt = dist[e.to] + 1;
                 }
             }
-            if (excess[i] > 0) {
-                dist[i] = dnxt;
-                activate(i);
-            }
+            if (excess[i] > 0) pque.push(i, dist[i] = dnxt);
             if (--tick == 0) tick = _n, global_relabeling(t);
         }
         return excess[t] + INF;
@@ -98,6 +97,6 @@ struct mf_pushrelabel {
         Cap delta = e.cap < excess[i] ? e.cap : excess[i];
         excess[i] -= delta, e.cap -= delta;
         excess[e.to] += delta, g[e.to][e.rev].cap += delta;
-        if (excess[e.to] > 0 and excess[e.to] <= delta) activate(e.to);
+        if (excess[e.to] > 0 and excess[e.to] <= delta) pque.push(e.to, dist[e.to]);
     }
 };
