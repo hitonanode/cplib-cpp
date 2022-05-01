6c561aa6e5faf24a90701c724e7bfc3c8461bb25
hitonanode <32937551+hitonanode@users.noreply.github.com>
2022-05-01 15:34:14 +0900

    rotate90() / transpose(): vector of string can be used

--- utilities/transpose.hpp
+++ utilities/transpose.hpp
@@ -1,9 +1,19 @@
+#pragma once
+#include <string>
 #include <vector>
 
-// CUT begin
-template <typename T> std::vector<std::vector<T>> trans(std::vector<std::vector<T>> v) {
-    int H = v.size(), W = v[0].size();
+template <typename T> std::vector<std::vector<T>> transpose(const std::vector<std::vector<T>> &v) {
+    const int H = v.size(), W = v[0].size();
     std::vector<std::vector<T>> ret(W, std::vector<T>(H));
+    for (int i = 0; i < H; i++) {
+        for (int j = 0; j < W; j++) ret[j][i] = v[i][j];
+    }
+    return ret;
+}
+
+std::vector<std::string> transpose(const std::vector<std::string> &v) {
+    const int H = v.size(), W = v[0].size();
+    std::vector<std::string> ret(W, std::string(H, '\0'));
     for (int i = 0; i < H; i++) {
         for (int j = 0; j < W; j++) ret[j][i] = v[i][j];
     }
