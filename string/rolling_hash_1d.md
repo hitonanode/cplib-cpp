68a006479bf417506e0cda3efd49dea7c3ff5010
Ryotaro Sato <32937551+hitonanode@users.noreply.github.com>
2022-10-09 14:11:30 +0900

    rolling hash (2d) (#238)
    * rolling hash (2d)
    
    * [auto-verifier] verify commit d16b8f07ba984630a8fcf93843c7dee17415b642
    
    Co-authored-by: GitHub <noreply@github.com>

--- string/rolling_hash_1d.md
+++ string/rolling_hash_1d.md
@@ -5,6 +5,14 @@ documentation_of: ./rolling_hash_1d.hpp
 
 文字列・数列に対する典型的なローリングハッシュ．長さ $N$ の入力に対して時間計算量 $O(N)$ で構築，区間ハッシュ値を $O(1)$ で取得．
 
+算出されるハッシュ値は，コンストラクタでランダムにとった `Hash b` とクエリ $(l, r)$ に対して
+
+$\displaystyle
+\mathrm{hash}(l, r) = \sum\_{i=l}^{r - 1} b^{r - 1 - i} s[i]
+$
+
+と表される．
+
 ## 使用方法
 
 ```cpp
@@ -17,13 +25,14 @@ int l, r;
 Hash h = rh.get(l, r);  // S[l:r], 半開区間
 ```
 
-ハッシュの型 `PairHash` や `TupleHash3` は入れ子にすることも可能で，例えば
+ハッシュの型 `PairHash` や `TupleHash3` は任意に入れ子にすることも可能で，例えば
 
 ```cpp
 using Hash = ModIntMersenne61;
-using Hash = TupleHash3<ModInt998244353, ModInt998244353, ModInt998244353>>;
-using Hash = TupleHash3<PH, ModInt998244353, PH>>;
-using Hash = PairHash<PH, PairHash<ModInt998244353, ModInt998244353>>>;
+using Hash = TupleHash3<ModInt998244353, ModInt998244353, ModInt998244353>;
+using PH = PairHash<ModInt998244353, ModInt998244353>;
+using Hash = TupleHash3<PH, ModInt998244353, PH>;
+using Hash = PairHash<PH, PairHash<ModInt998244353, ModInt998244353>>;
 ```
 
 は全て動作する．
---
+++ string/rolling_hash_2d.hpp
@@ -0,0 +1,59 @@
+#pragma once
+#include <string>
+#include <vector>
+
+
+// Rolling Hash (Rabin-Karp), 2dim
+template <typename V> struct rolling_hash_2d {
+    const V Bx, By;
+    std::vector<V> powx, powy;  // powx[i] = Bx^i
+    std::vector<std::vector<V>> hash;
+
+    void gen_pow(int h, int w) {
+        powx.assign(h + 1, V(1));
+        for (int i = 1; i <= h; ++i) powx.at(i) = powx.at(i - 1) * Bx;
+        powy.assign(w + 1, V(1));
+        for (int i = 1; i <= w; ++i) powy.at(i) = powy.at(i - 1) * By;
+    }
+
+    inline V _at(int x, int y) const noexcept {
+        if (x < 0 or x >= int(hash.size())) return V();
+        if (y < 0 or y >= int(hash[x].size())) return V();
+        return hash[x][y];
+    }
+
+    template <typename Int>
+    void build(const std::vector<std::vector<Int>> &s) {
+        const int H = s.size(), W = H ? s.at(0).size() : 0;
+        gen_pow(H, W);
+
+        hash.assign(H, std::vector<V>(W, V()));
+        for (int i = 0; i < H; ++i) {
+            for (int j = 0; j < W; ++j) hash[i][j] = _at(i, j - 1) * By + s[i][j];
+        }
+
+        for (int i = 0; i < H; ++i) {
+            for (int j = 0; j < W; ++j) hash[i][j] = hash[i][j] + _at(i - 1, j) * Bx;
+        }
+    }
+
+    template <typename Int>
+    rolling_hash_2d(const std::vector<std::vector<Int>> &s, V bx, V by) : Bx(bx), By(by) {
+        build(s);
+    }
+
+    rolling_hash_2d(const std::vector<std::string> &m, V bx, V by) : Bx(bx), By(by) {
+        std::vector<std::vector<int>> v_;
+        for (const auto &s : m) {
+            v_.push_back({});
+            for (char c : s) v_.back().push_back(int(c));
+        }
+        build(v_);
+    }
+
+    V get(int xl, int xr, int yl, int yr) const {
+        return _at(xr - 1, yr - 1) - _at(xl - 1, yr - 1) * powx[xr - xl] -
+               _at(xr - 1, yl - 1) * powy[yr - yl] +
+               _at(xl - 1, yl - 1) * powx[xr - xl] * powy[yr - yl];
+    }
+};
