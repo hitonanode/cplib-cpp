c988bee5be5b86cd18f6e538e12e0a4fbcdf104a
hitonanode <32937551+hitonanode@users.noreply.github.com>
2023-05-02 02:06:50 +0900

    Fix shortest_cycle01

--- graph/shortest_cycle01.hpp
+++ graph/shortest_cycle01.hpp
@@ -5,13 +5,12 @@
 #include <utility>
 #include <vector>
 
-// CUT begin
 // Shortest cycle detection of UNDIRECTED SIMPLE graphs based on 01-BFS
 // Assumption: only two types of edges are permitted: weight = 0 or W ( > 0)
 // Complexity: O(E)
-// Verified: <https://codeforces.com/contest/1325/problem/E>
+// Verified: https://codeforces.com/contest/1325/problem/E https://codeforces.com/contest/1817/problem/B
 struct ShortestCycle01 {
-    const int INF = std::numeric_limits<int>::max() / 2;
+    static constexpr int INF = std::numeric_limits<int>::max() / 2;
     int V, E;
     int INVALID = -1;
     std::vector<std::vector<std::pair<int, int>>> to; // (nxt, weight)
@@ -28,6 +27,7 @@ struct ShortestCycle01 {
 
     std::vector<int> dist;
     std::vector<int> prev;
+    std::vector<int> orig;
     // Find minimum length simple cycle which passes vertex `v`
     // - return: (LEN, (a, b))
     //   - LEN: length of the shortest cycles if exists, INF otherwise.
@@ -37,12 +37,20 @@ struct ShortestCycle01 {
         dist.assign(V, INF);
         dist[v] = 0;
         prev.assign(V, -1);
+        orig.assign(V, -1);
         std::deque<std::pair<int, int>> bfsq;
         std::vector<std::pair<std::pair<int, int>, int>> add_edge;
         bfsq.emplace_back(v, -1);
         while (!bfsq.empty()) {
             int now = bfsq.front().first, prv = bfsq.front().second;
             bfsq.pop_front();
+            if (prv < 0) {
+                //
+            } else if (prv == v) {
+                orig.at(now) = now;
+            } else {
+                orig.at(now) = orig.at(prv);
+            }
             for (auto nxt : to[now])
                 if (nxt.first != prv) {
                     if (dist[nxt.first] == INF) {
@@ -60,6 +68,7 @@ struct ShortestCycle01 {
         int s = -1, t = -1;
         for (auto edge : add_edge) {
             int a = edge.first.first, b = edge.first.second;
+            if (orig.at(a) == orig.at(b)) continue;
             int L = dist[a] + dist[b] + edge.second;
             if (L < minimum_cycle) minimum_cycle = L, s = a, t = b;
         }
