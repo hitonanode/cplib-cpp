a32e95cf12f6f70832348ae75b5f13ae3f1efe20
hitonanode <32937551+hitonanode@users.noreply.github.com>
2023-12-09 22:52:11 +0900

    update light trie

--- string/trie_light.hpp
+++ string/trie_light.hpp
@@ -1,26 +1,36 @@
 #include <string>
 #include <vector>
 
-// CUT begin
 struct Trie {
     char a_init;
     int D;
     int INVALID = -1;
     std::vector<std::vector<int>> child;
+    std::vector<int> par;
+
     using T_NODE = int;
     std::vector<T_NODE> v_info;
+
     Trie(char a_init = 'a', int D = 26)
-        : a_init(a_init), D(D), child(1, std::vector<int>(D, INVALID)), v_info(1) {}
-    void add_word(const std::string &str, T_NODE info) {
+        : a_init(a_init), D(D), child(1, std::vector<int>(D, INVALID)), par(1, -1), v_info(1) {}
+
+    int step(int now, char c) const {
+        if (now == INVALID) return INVALID;
+        return child.at(now).at(c - a_init);
+    }
+
+    int add_word(const std::string &str, T_NODE info) {
         int now = 0;
         for (auto &c : str) {
             if (child[now][c - a_init] == INVALID) {
+                par.push_back(now);
                 child[now][c - a_init] = child.size();
-                child.emplace_back(std::vector<int>(D, INVALID));
+                child.emplace_back(D, INVALID);
                 v_info.resize(child.size());
             }
             now = child[now][c - a_init];
         }
         v_info[now] += info;
+        return now;
     }
 };
