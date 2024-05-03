64c16f372557ae1c27d80610c5524638e1973ce1
hitonanode <32937551+hitonanode@users.noreply.github.com>
2024-05-03 13:54:12 +0900

    Add F2 intersection

---
+++ linear_algebra_matrix/test/f2_intersection.test.cpp
@@ -0,0 +1,31 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/intersection_of_f2_vector_spaces"
+#include "../f2_vector_space.hpp"
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int n;
+        cin >> n;
+        vector<int> A(n);
+        for (int &x : A) cin >> x;
+
+        int m;
+        cin >> m;
+        vector<int> B(m);
+        for (int &x : B) cin >> x;
+
+        auto C = f2_intersection(A, B);
+
+        cout << C.size();
+        for (int x : C) cout << ' ' << x;
+        cout << '\n';
+    }
+}
