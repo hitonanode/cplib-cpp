49a5c004899a1719a0174a429da054507ba316c6
hitonanode <32937551+hitonanode@users.noreply.github.com>
2025-10-26 14:03:13 +0900

    refactor beats

--- segmenttree/test/beats_range-chmax-add-range-sum.aoj0427.test.cpp
+++ segmenttree/test/beats_range-chmax-add-range-sum.aoj0427.test.cpp
@@ -1,17 +1,19 @@
-#include "../trees/acl_range-add-chmax-range-sum.hpp"
+#include "../trees/range-chmax-add-range-sum.hpp"
 #define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=0427"
 #include <iostream>
 using namespace std;
+
+using RCARS = RangeChmaxAddRangeSum<long long, (1LL << 60)>;
 
 int main() {
     cin.tie(nullptr), ios::sync_with_stdio(false);
 
     int N, tmp;
     cin >> N;
-    vector<RangeAddChmaxRangeSum::S> A(N);
-    for (auto &a : A) cin >> tmp, a = tmp;
+    vector<RCARS::S> A(N);
+    for (auto &a : A) cin >> tmp, a = RCARS::Gen(tmp);
 
-    RangeAddChmaxRangeSum::segtree segtree(A);
+    RCARS::segtree segtree(A);
 
     int Q, q, l, r, d;
     cin >> Q;
@@ -20,12 +22,12 @@ int main() {
         cin >> q >> l >> r >> d;
         l--;
         if (q == 1) {
-            segtree.apply(l, r, RangeAddChmaxRangeSum::F::add(d));
+            segtree.apply(l, r, RCARS::Add(d));
         } else {
             auto before = segtree.prod(l, r).sum;
-            auto f1 = RangeAddChmaxRangeSum::F::add(-d);
-            auto f2 = RangeAddChmaxRangeSum::F::chmax(0);
-            segtree.apply(l, r, RangeAddChmaxRangeSum::composition(f2, f1));
+            auto f1 = RCARS::Add(-d);
+            auto f2 = RCARS::Chmax(0);
+            segtree.apply(l, r, RCARS::composition(f2, f1));
             auto after = segtree.prod(l, r).sum;
             cout << before - after << '\n';
         }
