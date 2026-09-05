545f43a98ff89040873b147548cfc4236a7e91dc
hitonanode <32937551+hitonanode@users.noreply.github.com>
2026-09-05 15:18:54 +0900

    Fix shortest zero-one cycle

--- graph/shortest_cycle01.hpp
+++ graph/shortest_cycle01.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <algorithm>
 #include <cassert>
 #include <deque>
 #include <limits>
@@ -35,43 +36,90 @@ struct ShortestCycle01 {
     std::pair<int, std::pair<int, int>> Solve(int v) {
         assert(0 <= v and v < V);
         dist.assign(V, INF);
-        dist[v] = 0;
         prev.assign(V, -1);
         orig.assign(V, -1);
-        std::deque<std::pair<int, int>> bfsq;
-        std::vector<std::pair<std::pair<int, int>, int>> add_edge;
-        bfsq.emplace_back(v, -1);
-        while (!bfsq.empty()) {
-            int now = bfsq.front().first, prv = bfsq.front().second;
-            bfsq.pop_front();
-            if (prv < 0) {
-                //
-            } else if (prv == v) {
-                orig.at(now) = now;
-            } else {
-                orig.at(now) = orig.at(prv);
-            }
-            for (auto nxt : to[now])
-                if (nxt.first != prv) {
-                    if (dist[nxt.first] == INF) {
-                        dist[nxt.first] = dist[now] + nxt.second;
-                        prev[nxt.first] = now;
-                        if (nxt.second)
-                            bfsq.emplace_back(nxt.first, now);
-                        else
-                            bfsq.emplace_front(nxt.first, now);
-                    } else
-                        add_edge.emplace_back(std::make_pair(now, nxt.first), nxt.second);
-                }
+        std::vector<int> root_weight(V, INF);
+        std::deque<int> bfsq;
+        for (auto [nxt, weight] : to[v]) {
+            if (weight >= dist[nxt]) continue;
+            dist[nxt] = weight;
+            root_weight[nxt] = weight;
+            prev[nxt] = v;
+            orig[nxt] = nxt;
+            if (weight)
+                bfsq.emplace_back(nxt);
+            else
+                bfsq.emplace_front(nxt);
         }
         int minimum_cycle = INF;
-        int s = -1, t = -1;
-        for (auto edge : add_edge) {
-            int a = edge.first.first, b = edge.first.second;
-            if (orig.at(a) == orig.at(b)) continue;
-            int L = dist[a] + dist[b] + edge.second;
-            if (L < minimum_cycle) minimum_cycle = L, s = a, t = b;
+        int source_a = -1, source_b = -1;
+        while (!bfsq.empty()) {
+            int now = bfsq.front();
+            bfsq.pop_front();
+            for (auto [nxt, weight] : to[now]) {
+                if (nxt == v) continue;
+                if (orig[nxt] >= 0 and orig[now] != orig[nxt]) {
+                    int length = dist[now] + dist[nxt] + weight;
+                    if (length < minimum_cycle) {
+                        minimum_cycle = length;
+                        source_a = orig[now], source_b = orig[nxt];
+                    }
+                }
+                int dnext = dist[now] + weight;
+                if (dnext < dist[nxt]) {
+                    dist[nxt] = dnext;
+                    prev[nxt] = now;
+                    orig[nxt] = orig[now];
+                    if (weight)
+                        bfsq.emplace_back(nxt);
+                    else
+                        bfsq.emplace_front(nxt);
+                }
+            }
         }
-        return std::make_pair(minimum_cycle, std::make_pair(s, t));
+        if (source_a < 0) return std::make_pair(INF, std::make_pair(-1, -1));
+
+        std::vector<int> path_dist(V, INF), path_prev(V, -1);
+        path_dist[source_a] = 0;
+        bfsq = {source_a};
+        while (!bfsq.empty()) {
+            int now = bfsq.front();
+            bfsq.pop_front();
+            for (auto [nxt, weight] : to[now]) {
+                if (nxt == v) continue;
+                int dnext = path_dist[now] + weight;
+                if (dnext < path_dist[nxt]) {
+                    path_dist[nxt] = dnext;
+                    path_prev[nxt] = now;
+                    if (weight)
+                        bfsq.emplace_back(nxt);
+                    else
+                        bfsq.emplace_front(nxt);
+                }
+            }
+        }
+        std::vector<int> path;
+        for (int x = source_b; x >= 0; x = path_prev[x]) {
+            path.push_back(x);
+            if (x == source_a) break;
+        }
+        std::reverse(path.begin(), path.end());
+        int split = (path.size() - 1) / 2;
+        prev[path.front()] = v;
+        orig[path.front()] = path.front();
+        dist[path.front()] = root_weight[path.front()];
+        for (int i = 1; i <= split; i++) {
+            prev[path[i]] = path[i - 1];
+            orig[path[i]] = path.front();
+        }
+        prev[path.back()] = v;
+        orig[path.back()] = path.back();
+        dist[path.back()] = root_weight[path.back()];
+        for (int i = path.size() - 2; i > split; i--) {
+            prev[path[i]] = path[i + 1];
+            orig[path[i]] = path.back();
+        }
+        minimum_cycle = root_weight[source_a] + path_dist[source_b] + root_weight[source_b];
+        return std::make_pair(minimum_cycle, std::make_pair(path[split], path[split + 1]));
     }
 };
